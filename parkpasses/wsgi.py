d6535a92b070712cb31683fc6137ae47165959ea
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-08 12:03:52 +0800

    Configuring project template

--- parkpasses/wsgi.py
+++ parkpasses/wsgi.py
@@ -3,16 +3,13 @@ WSGI config for ledger project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 """
 import os
-from django.core.wsgi import get_wsgi_application
-
-# from dj_static import Cling, MediaCling
 
 import confy
+from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 confy.read_environment_file(BASE_DIR + "/.env")
 os.environ.setdefault("BASE_DIR", BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "parkpasses.settings")
-# application = Cling(MediaCling(get_wsgi_application()))
 application = get_wsgi_application()
