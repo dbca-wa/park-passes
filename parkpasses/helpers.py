7cc1e34723e5f911dde9f97309be0bbd98171fb2
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-08 12:23:48 +0800

    Configuring project template

--- parkpasses/helpers.py
+++ parkpasses/helpers.py
@@ -1,12 +1,12 @@
-from __future__ import unicode_literals
-from ledger_api_client.ledger_models import EmailUserRO as EmailUser, EmailUserRO
-from ledger_api_client.managed_models import SystemGroup
-from django.conf import settings
-from django.core.cache import cache
-
 import logging
 
-from parkpasses.settings import GROUP_NAME_ASSESSOR, GROUP_NAME_APPROVER
+from django.conf import settings
+from django.core.cache import cache
+from ledger_api_client.ledger_models import EmailUserRO
+from ledger_api_client.ledger_models import EmailUserRO as EmailUser
+from ledger_api_client.managed_models import SystemGroup
+
+from parkpasses.settings import GROUP_NAME_APPROVER, GROUP_NAME_ASSESSOR
 
 logger = logging.getLogger(__name__)
 
@@ -35,17 +35,6 @@ def belongs_to(user, group_name):
         )
     return belongs_to_value
 
-    # return user.groups.filter(name=group_name).exists()
-
-
-# def is_model_backend(request):
-#    # Return True if user logged in via single sign-on (i.e. an internal)
-#    return 'ModelBackend' in request.session.get('_auth_user_backend')
-#
-# def is_email_auth_backend(request):
-#    # Return True if user logged in via social_auth (i.e. an external user signing in with a login-token)
-#    return 'EmailAuth' in request.session.get('_auth_user_backend')
-
 
 def is_parkpasses_admin(request):
     # logger.info('settings.ADMIN_GROUP: {}'.format(settings.ADMIN_GROUP))
@@ -72,18 +61,6 @@ def in_dbca_domain(request):
     return request.user.is_staff
 
 
-#    #import ipdb; ipdb.set_trace()
-#    user = request.user
-#    domain = user.email.split('@')[1]
-#    if domain in settings.DEPT_DOMAINS:
-#        if not user.is_staff:
-#            # hack to reset department user to is_staff==True, if the user logged in externally (external departmentUser login defaults to is_staff=False)
-#            user.is_staff = True
-#            user.save()
-#        return True
-#    return False
-
-
 def is_in_organisation_contacts(request, organisation):
     return request.user.email in organisation.contacts.all().values_list(
         "email", flat=True
@@ -91,12 +68,10 @@ def is_in_organisation_contacts(request, organisation):
 
 
 def is_departmentUser(request):
-    # return request.user.is_authenticated and is_model_backend(request) and in_dbca_domain(request)
     return request.user.is_authenticated and request.user.is_staff
 
 
 def is_customer(request):
-    # return request.user.is_authenticated and is_email_auth_backend(request)
     return request.user.is_authenticated and not request.user.is_staff
 
 
