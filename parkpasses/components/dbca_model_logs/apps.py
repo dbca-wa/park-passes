f98fdc0d881d3dbc006f5d905ce6aae16af5dd82
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-13 13:40:53 +0800

    Changed component prefix to org

--- parkpasses/components/dbca_model_logs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DbcaModelLogsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "parkpasses.components.dbca_model_logs"
