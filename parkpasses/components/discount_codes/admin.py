44a77b8a9a1491d5235870e8b5061efc640d7b17
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-14 15:09:58 +0800

    Basic config for discount codes admin

--- parkpasses/components/discount_codes/admin.py
+++ parkpasses/components/discount_codes/admin.py
@@ -1,20 +1,42 @@
 from django.contrib import admin
 
-from parkpasses.components.discount_codes.models import DiscountCodeBatch
+from parkpasses.components.discount_codes.models import DiscountCode, DiscountCodeBatch
+
+
+class DiscountCodeAdmin(admin.TabularInline):
+    model = DiscountCode
 
 
 class DiscountCodeBatchAdmin(admin.ModelAdmin):
     model = DiscountCodeBatch
+    fields = (
+        "codes_to_generate",
+        "times_each_code_can_be_used",
+        "invalidated",
+        "discount_amount",
+        "discount_percentage",
+        "datetime_expiry",
+    )
     list_display = (
+        "discount_code_batch_number",
         "codes_to_generate",
         "times_each_code_can_be_used",
         "discount_amount",
         "discount_percentage",
     )
     readonly_fields = (
+        "codes_to_generate",
+        "times_each_code_can_be_used",
+        "discount_code_batch_number",
         "datetime_created",
         "datetime_updated",
     )
+    inlines = [DiscountCodeAdmin]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # Editing
+            return self.readonly_fields
+        return ()
 
 
 admin.site.register(DiscountCodeBatch, DiscountCodeBatchAdmin)
