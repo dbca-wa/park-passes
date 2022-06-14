c0d697dd25c4c8fdb7c26b7fc3407ed82a719690
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-14 15:07:40 +0800

    Fixed the debug logging configuration

--- parkpasses/settings.py
+++ parkpasses/settings.py
@@ -210,7 +210,7 @@ if DEBUG:
             },
         },
         "loggers": {
-            "disturbance": {
+            "parkpasses": {
                 "handlers": ["console"],
                 "level": "DEBUG",
                 "propagate": False,
