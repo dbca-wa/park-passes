2c75b804ed05bae7cee6f6d2f920de8d34232d88
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-15 12:21:19 +0800

    Added basic configation for pass type admin

--- parkpasses/components/passes/admin.py
+++ parkpasses/components/passes/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from parkpasses.components.passes.models import PassType
+
+
+class PassTypeAdmin(admin.ModelAdmin):
+    model = PassType
+    list_display = (
+        "name",
+        "display_name",
+        "display_order",
+        "display_externally",
+    )
+    ordering = [
+        "display_order",
+    ]
+    readonly_fields = [
+        "name",
+    ]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # Editing
+            return self.readonly_fields
+        return ()
+
+
+admin.site.register(PassType, PassTypeAdmin)
