e2395d82c3f42b6d8a16800c0db8fe43078fe7b6
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-21 16:31:18 +0800

    Added basic admin configuration

--- org_model_logs/admin.py
+++ org_model_logs/admin.py
@@ -1,3 +1,30 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from org_model_logs.models import CommunicationsLogEntry, UserAction
+
+
+class UserActionAdmin(admin.ModelAdmin):
+    model = UserAction
+    list_display = (
+        "object_id",
+        "content_type",
+        "who",
+        "when",
+        "what",
+    )
+    ordering = ["-when"]
+
+
+admin.site.register(UserAction, UserActionAdmin)
+
+
+class CommunicationsLogEntryAdmin(admin.ModelAdmin):
+    model = CommunicationsLogEntry
+    list_display = (
+        "object_id",
+        "content_type",
+    )
+    ordering = ["-created"]
+
+
+admin.site.register(CommunicationsLogEntry, CommunicationsLogEntryAdmin)
