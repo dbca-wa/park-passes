e157afc286a317ea516f389f312eb4c98fdb2c35
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-21 16:30:44 +0800

    Added verbose app name

--- org_model_documents/apps.py
+++ org_model_documents/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class OrgModelDocumentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "org_model_documents"
+    verbose_name = "Park Pass Model Documents"
