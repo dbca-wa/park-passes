cc0a284cc949b9d4ab784c9b1f6bf80852205d5c
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-21 14:57:57 +0800

    Further configuration of admin and models

--- parkpasses/components/cart/admin.py
+++ parkpasses/components/cart/admin.py
@@ -1,3 +1,28 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from parkpasses.components.cart.models import Cart, CartItem
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+
+
+class CartAdmin(admin.ModelAdmin):
+    model = Cart
+    list_display = (
+        "user",
+        "grand_total",
+        "datetime_created",
+        "datetime_first_added_to",
+        "datetime_last_added_to",
+    )
+    readonly_fields = [
+        "datetime_created",
+        "datetime_first_added_to",
+        "datetime_last_added_to",
+    ]
+    ordering = ("-datetime_created",)
+    inlines = [CartItemInline]
+
+
+admin.site.register(Cart, CartAdmin)
--- parkpasses/components/cart/models.py
+++ parkpasses/components/cart/models.py
@@ -1,8 +1,12 @@
 """
     This module contains the models required for implimenting the shopping cart
 """
+import logging
+
+from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.utils import timezone
 
 from parkpasses.components.discount_codes.models import DiscountCode
 from parkpasses.components.passes.models import Pass
@@ -10,26 +14,53 @@ from parkpasses.components.users.models import UserInformation
 from parkpasses.components.vouchers.models import Voucher
 from parkpasses.ledger_api_utils import retrieve_email_user
 
+logger = logging.getLogger(__name__)
+
+
+class CartManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related("items")
+
 
 class Cart(models.Model):
     """A class to represent a cart"""
 
     user = models.IntegerField(null=False, blank=False)  # EmailUserRO
-    datetime_first_added_to = models.DateTimeField()
-    datetime_last_added_to = models.DateTimeField()
+    datetime_created = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    datetime_first_added_to = models.DateTimeField(null=True, blank=True)
+    datetime_last_added_to = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         app_label = "parkpasses"
 
+    def __str__(self):
+        return f"Cart for user: {self.user} (datetime_created {self.datetime_created})"
+
     @property
-    def user(self):
+    def email_user(self):
         return retrieve_email_user(self.user)
+
+    @property
+    def grand_total(self):
+        grand_total = 0.00
+        for item in self.items.all():
+            grand_total += item.get_total_price()
+        return grand_total
+
+
+class CartItemManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().select_related("cart")
 
 
 class CartItem(models.Model):
     """A class to represent a cart item"""
 
-    cart = models.ForeignKey(Cart, on_delete=models.PROTECT, null=False, blank=False)
+    objects = CartItemManager()
+
+    cart = models.ForeignKey(
+        Cart, related_name="items", on_delete=models.PROTECT, null=False, blank=False
+    )
     object_id = models.CharField(max_length=191)  # voucher or pass id
     content_type = models.ForeignKey(
         ContentType, on_delete=models.CASCADE
@@ -42,43 +73,48 @@ class CartItem(models.Model):
     )
 
     class Meta:
-        """Meta for cart item - used here to add a custom constraint
-
-        A cart item must have either a park pass or a voucher attached to
-        be valid.
-        """
-
         app_label = "parkpasses"
-        constraints = [
-            models.CheckConstraint(
-                name="%(app_label)s_%(class)s_park_pass_or_voucher",
-                check=(
-                    models.Q(park_pass__isnull=True, voucher__isnull=False)
-                    | models.Q(park_pass__isnull=False, voucher__isnull=True)
-                ),
-            )
-        ]
+
+    def __str__(self):
+        return f"Content Type: {self.content_type} | Object ID: {self.object_id} Total Price: {self.get_total_price()}"
+
+    def save(self, *args, **kwargs):
+        logger.debug(
+            "PARKPASSES_VALID_CART_CONTENT_TYPES = "
+            + str(settings.PARKPASSES_VALID_CART_CONTENT_TYPES)
+        )
+        logger.debug("self.content_type = " + str(self.content_type))
+        if str(self.content_type) not in settings.PARKPASSES_VALID_CART_CONTENT_TYPES:
+            raise ValueError("A Cart Item can only contain a Voucher or a Pass")
+        datetime_item_added = timezone.now()
+        logger.debug("self.cart.items = " + str(self.cart.items))
+        if not self.cart.datetime_first_added_to and not self.cart.items.count():
+            self.cart.datetime_first_added_to = datetime_item_added
+        self.cart.datetime_last_added_to = datetime_item_added
+        self.cart.save()
+        super().save(*args, **kwargs)
 
     def get_total_price(self):
-        model_type = self.content_type.model
-        if "Voucher" == model_type:
+        model_type = str(self.content_type)
+        logger.debug("model_type = " + str(model_type))
+        if "parkpasses | voucher" == model_type:
             return Voucher.objects.get(pk=self.object_id).amount
-        elif "Pass" == model_type:
-            return self.get_total_price_pass()
-        else:
-            raise ValueError("A Cart Item can only contain a Voucher or a Pass")
+        elif "parkpasses | pass" == model_type:
+            return float(self.get_total_price_pass())
 
     def get_total_price_pass(self):
         park_pass = Pass.objects.get(pk=self.object_id)
         total_price = park_pass.option.price
-        user_information = UserInformation.objects.get(pk=self.cart.user)
-        concession = user_information.concession
-        if concession:
-            concession_percentage = concession.discount_percentage
-            concession_discount = total_price * (concession_percentage / 100)
-            total_price += concession_discount
+        if UserInformation.objects.filter(user=self.cart.user).count():
+            user_information = UserInformation.objects.get(user=self.cart.user)
+            concession = user_information.concession
+            if concession:
+                concession_percentage = concession.discount_percentage
+                concession_discount = total_price * (concession_percentage / 100)
+                total_price += concession_discount
 
         if self.discount_code:
+            logger.debug("self.discount_code = " + str(self.discount_code))
             discount_code_batch = self.discount_code.discount_code_batch
             if discount_code_batch.discount_percentage:
                 discount_code_discount = total_price * (
@@ -89,8 +125,9 @@ class CartItem(models.Model):
                     total_price - discount_code_batch.discount_amount
                 )
 
-            if total_price - discount_code_discount < 0.00:
-                return 0.00
+                if total_price - discount_code_discount < 0.00:
+                    return 0.00
+            total_price -= discount_code_discount
 
         if self.voucher:
             if self.voucher.amount >= total_price:
