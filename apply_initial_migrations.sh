defe89351517ae83f6e35df199cd711d8c55cd66
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-22 15:06:54 +0800

    New version that can handle poetry as in virtual env and without virtual env

--- apply_initial_migrations.sh
+++ apply_initial_migrations.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-poetry run python manage.py migrate auth &&
-poetry run python manage.py migrate ledger_api_client &&
+POETRY_ENV=$(poetry env info -p)
+if [ $POETRY_ENV ]; then
+    RUN_PYTHON="poetry run python"
+else
+    RUN_PYTHON="python"
+fi
+$RUN_PYTHON manage.py migrate auth &&
+$RUN_PYTHON manage.py migrate ledger_api_client &&
 
 # patch admin 0001_initial migration file
-POETRY_ENV=$(poetry env info -p)
 echo "POETRY_ENV"
 echo $POETRY_ENV
 if [ $POETRY_ENV ]; then
@@ -23,7 +28,7 @@ else
         fi
 fi
 
-poetry run python manage.py migrate admin &&
+$RUN_PYTHON manage.py migrate admin &&
 
 # repatch admin 0001_initial migration file
 if [ $POETRY_ENV ]; then
@@ -44,10 +49,10 @@ else
         fi
 fi
 
-poetry run python manage.py migrate django_cron &&
-poetry run python manage.py migrate sites 0001_initial &&
-poetry run python manage.py migrate flatpages 0001_initial &&
-poetry run python manage.py migrate sites 0002_alter_domain_unique &&
-poetry run python manage.py migrate sessions &&
-poetry run python manage.py migrate &&
-poetry run python manage.py dbshell -- -c 'ALTER TABLE django_admin_log RENAME COLUMN "user" TO "user_id";'
+ $RUN_PYTHON manage.py migrate django_cron &&
+ $RUN_PYTHON manage.py migrate sites 0001_initial &&
+ $RUN_PYTHON manage.py migrate flatpages 0001_initial &&
+ $RUN_PYTHON manage.py migrate sites 0002_alter_domain_unique &&
+ $RUN_PYTHON manage.py migrate sessions &&
+ $RUN_PYTHON manage.py migrate &&
+ $RUN_PYTHON manage.py dbshell -- -c 'ALTER TABLE django_admin_log RENAME COLUMN "user" TO "user_id";'
