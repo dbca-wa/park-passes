0dafd4f8061c89bc3b7b252387c137213719730a
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-23 10:31:13 +0800

    Added help app to project

--- parkpasses/components/help/admin.py
+++ parkpasses/components/help/admin.py
@@ -1,3 +1,19 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from parkpasses.components.help.models import HelpText
+
+
+class HelpTextAdmin(admin.ModelAdmin):
+    model = HelpText
+    list_display = ("label", "version")
+    readonly_fields = [
+        "content",
+    ]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # Editing
+            return self.readonly_fields
+        return ()
+
+
+admin.site.register(HelpText, HelpTextAdmin)
--- parkpasses/components/help/models.py
+++ parkpasses/components/help/models.py
@@ -1,3 +1,21 @@
-# from django.db import models
+"""
+    This module contains the models for implimenting help messages.
+"""
+from ckeditor.fields import RichTextField
+from django.db import models
 
-# Create your models here.
+
+class HelpText(models.Model):
+    """A class to represent a help text item"""
+
+    label = models.CharField(unique=True, max_length=50, null=False, blank=False)
+    content = RichTextField()
+    version = models.SmallIntegerField(default=1, blank=False, null=False)
+
+    def __str__(self):
+        return f"Help Text Item for {self.label} (Version: {self.version})"
+
+    class Meta:
+        app_label = "parkpasses"
+        verbose_name = "Help Text"
+        verbose_name_plural = "Help Text"
