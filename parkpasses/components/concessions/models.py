1b8a613efe83517b2a1f6f4770de8c6eff7c2bfc
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-23 15:54:14 +0800

    Made concession_type not nullable

--- parkpasses/components/concessions/models.py
+++ parkpasses/components/concessions/models.py
@@ -10,7 +10,9 @@ PERCENTAGE_VALIDATOR = [MinValueValidator(0), MaxValueValidator(100)]
 class Concession(models.Model):
     """A class to represent a concession"""
 
-    concession_type = models.CharField(unique=True, max_length=50)
+    concession_type = models.CharField(
+        unique=True, max_length=50, null=False, blank=False
+    )
     discount_percentage = models.DecimalField(
         max_digits=2,
         decimal_places=0,
