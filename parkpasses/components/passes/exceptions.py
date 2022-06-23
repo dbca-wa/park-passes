590c70cc40425c6b652dbd00363791d08be0dc28
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-23 09:55:42 +0800

    Removed log statement from exception class

--- parkpasses/components/passes/exceptions.py
+++ parkpasses/components/passes/exceptions.py
@@ -6,7 +6,3 @@ logger = logging.getLogger(__name__)
 
 class PassTemplateDoesNotExist(Exception):
     """The exception to be thrown if there is no pass template"""
-
-    logger.critical(
-        "CRITICAL: The system can not find a Pass Template to use for generating park passes."
-    )
