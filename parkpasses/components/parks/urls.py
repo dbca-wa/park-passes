c282c7a49ba41296e9c2906070e9e4c563aea375
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-27 09:14:03 +0800

    Added park and lga routes

--- parkpasses/components/parks/urls.py
+++ parkpasses/components/parks/urls.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
 
-from parkpasses.components.parks.api import PostcodeViewSet
+from parkpasses.components.parks.api import LGAViewSet, ParkViewSet, PostcodeViewSet
 
 router = routers.SimpleRouter()
 router.register(r"postcodes", PostcodeViewSet, basename="postcodes")
+router.register(r"parks", ParkViewSet, basename="parks")
+router.register(r"lgas", LGAViewSet, basename="lgas")
 
 urlpatterns = router.urls
