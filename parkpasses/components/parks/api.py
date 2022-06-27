3d9ea8f7b706fdbe118264b9554c336a46663000
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-06-27 09:13:35 +0800

    Added viewset for Park and LGA

--- parkpasses/components/parks/api.py
+++ parkpasses/components/parks/api.py
@@ -2,8 +2,12 @@ import logging
 
 from rest_framework import viewsets
 
-from parkpasses.components.parks.models import Postcode
-from parkpasses.components.parks.serializers import PostcodeSerializer
+from parkpasses.components.parks.models import LGA, Park, Postcode
+from parkpasses.components.parks.serializers import (
+    LGASerializer,
+    ParkSerializer,
+    PostcodeSerializer,
+)
 from parkpasses.helpers import is_internal
 
 logger = logging.getLogger(__name__)
@@ -19,6 +23,40 @@ class PostcodeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Postcode.objects.all()
+
+    def has_permission(self, request, view):
+        if is_internal(request):
+            return True
+        return False
+
+
+class ParkViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for performing actions on parks / park groups.
+    """
+
+    model = Park
+    serializer_class = ParkSerializer
+
+    def get_queryset(self):
+        return Park.objects.all()
+
+    def has_permission(self, request, view):
+        if is_internal(request):
+            return True
+        return False
+
+
+class LGAViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for performing actions on LGAs.
+    """
+
+    model = LGA
+    serializer_class = LGASerializer
+
+    def get_queryset(self):
+        return LGA.objects.all()
 
     def has_permission(self, request, view):
         if is_internal(request):
