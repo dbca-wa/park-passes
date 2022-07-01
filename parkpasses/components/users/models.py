169a117859e24fb28ecb9772dd94ba559f61fc0e
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-01 09:48:02 +0800

    Added concession_card_number field. Made user unqiue. Added class method to get the concession for a user by their user id

--- parkpasses/components/users/models.py
+++ parkpasses/components/users/models.py
@@ -13,10 +13,11 @@ from parkpasses.ledger_api_utils import retrieve_email_user
 class UserInformation(models.Model):
     """A class to store any additional user data that is needed specific to parks passes"""
 
-    user = models.IntegerField(null=False, blank=False)  # EmailUserRO
+    user = models.IntegerField(unique=True, null=False, blank=False)  # EmailUserRO
     concession = models.ForeignKey(
         Concession, on_delete=models.PROTECT, blank=True, null=True
     )
+    concession_card_number = models.CharField(max_length=50, null=True, blank=True)
     # Any other park pass specific user data to go here
 
     class Meta:
@@ -26,3 +27,9 @@ class UserInformation(models.Model):
     @property
     def email_user(self):
         return retrieve_email_user(self.user)
+
+    @classmethod
+    def get_concession_by_user_id(self, user_id):
+        if UserInformation.objects.filter(user=user_id).count():
+            return UserInformation.objects.get(user=user_id).concession
+        return None
