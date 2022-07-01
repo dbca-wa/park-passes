a1cb77edb2589bbebdcdbdeacbe09e4fbbefbb77
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-01 14:35:47 +0800

    Added login form

--- parkpasses/frontend/parkpasses/src/components/external/ShopHome.vue
+++ parkpasses/frontend/parkpasses/src/components/external/ShopHome.vue
@@ -30,6 +30,39 @@
           <div class="card faq-card mb-3">
             <div class="row g-0">
               <div class="col-md-4">
+                <img src="https://picsum.photos/id/101/300/210" class="img-fluid rounded-start" alt="Log In">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Log In</h5>
+
+                  <div class="row g-3 align-items-center">
+                    <div class="col-auto">
+                      <label for="inputPassword6" class="col-form-label">Email</label>
+                    </div>
+                    <div class="col-auto">
+                      <input type="email" id="inputPassword6" class="form-control" autofocus>
+                    </div>
+                    <div class="col-auto">
+                      <button type="submit" class="btn btn-primary">Log In</button>
+                    </div>
+                  </div>
+
+                  <p class="card-text">
+                      <ul>
+                        <li>View and download your current park pass</li>
+                        <li>Update the vehicle details for your pass</li>
+                        <li>Update your other details</li>
+                      </ul>
+                    </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card faq-card mb-3">
+            <div class="row g-0">
+              <div class="col-md-4">
                 <img src="https://picsum.photos/id/8/300/170" class="img-fluid rounded-start" alt="Frequently Asked Questions">
               </div>
               <div class="col-md-8">
@@ -50,26 +83,6 @@
                 <div class="card-body">
                   <h5 class="card-title">Help</h5>
                   <p class="card-text">Click here for help if you want to update your vehicle details or if you did not receive your park pass.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card faq-card mb-3">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="https://picsum.photos/id/101/300/170" class="img-fluid rounded-start" alt="Log In">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Log In</h5>
-                  <p class="card-text">
-                      <ul>
-                        <li>View and download your current park pass</li>
-                        <li>Update the vehicle details for your pass</li>
-                        <li>Update your other details</li>
-                      </ul>
-                    </p>
                 </div>
               </div>
             </div>
@@ -111,6 +124,9 @@ export default {
                 this.errorMessage = "ERROR: Please try again in an hour.";
                 console.error("There was an error!", error);
             });
+        },
+        redirectToLoginPage: function() {
+            window.location.href = 'external/'
         }
     },
     created: function () {
@@ -121,7 +137,8 @@ export default {
 
 <style scoped>
 .card-text ul {
-  font-size:.7em;
+  margin:10px 0 0 0;
+  font-size:.9em;
 }
 
 .card {
