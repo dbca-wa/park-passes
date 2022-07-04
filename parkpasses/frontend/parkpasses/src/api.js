548245146f77882f08e85096e558cc8b0494c256
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-04 17:26:27 +0800

    Added new end points

--- parkpasses/frontend/parkpasses/src/api.js
+++ parkpasses/frontend/parkpasses/src/api.js
@@ -1,10 +1,40 @@
 
 module.exports = {
-    passesPaginatedList: '/api/passes/passes/', // both for external and internal
-    discard_proposal: function (id) {
-        return `/api/proposal/${id}.json`;
+
+    /* ========================= Concessions ============================================*/
+
+    concessions:                        "/api/concessions/concessions?format=json",
+    concession: function (id) {
+        return                          `/api/concessions/concessions/${id}/?format=json`;
     },
-    passTypesDistinct:"/api/passes/pass-types-distinct?format=json",
-    passProcessingStatusesDistinct:"/api/passes/pass-processing-statuses-distinct?format=json",
-    passTypes:"/api/passes/pass-types?format=json",
+
+    /* ========================= Passes ================================================*/
+
+    passesPaginatedList:                '/api/passes/passes/',
+    passProcessingStatusesDistinct:     "/api/passes/pass-processing-statuses-distinct?format=json",
+
+    /* ========================= Pass Types ============================================*/
+
+    passTypesDistinct:                  "/api/passes/pass-types-distinct?format=json",
+    passTypes:                          "/api/passes/pass-types?format=json",
+    passType: function (id) {
+        return                          `/api/passes/pass-types/${id}/?format=json`;
+    },
+
+    /* ========================= Pass Options ============================================*/
+
+    passOptions: function (id) {
+        return                          `/api/passes/pass-options-by-pass-type-id?pass_type_id=${id}`;
+    },
+
+    /* ========================= Discount Codes ============================================*/
+    isDiscountCodeValid: function (id) {
+        return                          `to be implimented`;
+    },
+
+    /* ========================= Vouchers ============================================*/
+    isVoucherValid: function (id) {
+        return                          `to be implimented`;
+    },
+
 }
