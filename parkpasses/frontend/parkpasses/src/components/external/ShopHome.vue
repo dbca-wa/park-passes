5239723bf5b2d33d6fb818f716a79705ada60209
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-06 15:04:28 +0800

    Moved the home content to it's own component

--- parkpasses/frontend/parkpasses/src/components/external/ShopHome.vue
+++ parkpasses/frontend/parkpasses/src/components/external/ShopHome.vue
@@ -4,84 +4,17 @@
 
       <div class="col-4">
 
-        <ShopSideMenu />
+        <ShopSideMenu @purchase-voucher="purchaseVoucher" @purchase-pass="purchasePass" />
 
       </div>
 
       <div class="col">
 
-        <h1>Park Passes</h1>
+        <HomeContent v-if="showHomeContent" />
 
-        <p class="lead">Your ticket to the natural beauty of Western Australia.</p>
+        <PurchaseVoucher v-if="showPurchaseVoucher" />
 
-        <p>Exploring Western Australia's stunning parks and reserves? Park passes offer both value for money and convenience.</p>
-
-        <p>And why not purchase a Gift Voucher? The perfect gift for that nature lover you want to surprise!</p>
-
-        <div class="home-options">
-
-          <div class="card faq-card mb-3">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="https://picsum.photos/id/101/300/210" class="img-fluid rounded-start" alt="Log In">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Log In</h5>
-
-                  <div class="row g-3 align-items-center">
-                    <div class="col-auto">
-                      <label for="inputPassword6" class="col-form-label">Email</label>
-                    </div>
-                    <div class="col-auto">
-                      <input type="email" id="inputPassword6" class="form-control" autofocus>
-                    </div>
-                    <div class="col-auto">
-                      <button type="submit" class="btn btn-primary">Log In</button>
-                    </div>
-                  </div>
-
-                  <p class="card-text">
-                      <ul>
-                        <li>View and download your current park pass</li>
-                        <li>Update the vehicle details for your pass</li>
-                        <li>Update your other details</li>
-                      </ul>
-                    </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div @click="redirectToFAQ()" class="card faq-card mb-3">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="https://picsum.photos/id/8/300/170" class="img-fluid rounded-start" alt="Frequently Asked Questions">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">FAQ</h5>
-                  <p class="card-text">Click here for answers to common questions.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div @click="redirectToHelp()" class="card faq-card mb-3">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="https://picsum.photos/id/7/300/170" class="img-fluid rounded-start" alt="Help">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Help</h5>
-                  <p class="card-text">Click here for help if you want to update your vehicle details or if you did not receive your park pass.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
+        <PurchasePass v-if="showPurchasePass" :passTypeId="passTypeId" :key="passTypeId" />
 
       </div>
     </div>
@@ -91,20 +24,42 @@
 <script>
 import { api_endpoints } from '@/utils/hooks'
 import ShopSideMenu from '@/components/external/ShopSideMenu.vue'
+import HomeContent from '@/components/external/HomeContent.vue'
+import PurchaseVoucher from '@/components/external/PurchaseVoucher.vue'
+import PurchasePass from '@/components/external/PurchasePass.vue'
 
 export default {
     name: "ShopHome",
     data: function () {
         return {
-            passTypes: [],
+            passTypeId: -1,
+            showHomeContent: true,
+            showPurchaseVoucher: false,
+            showPurchasePass: false,
             errorMessage: null
         };
     },
     components: {
         api_endpoints,
         ShopSideMenu,
+        HomeContent,
+        PurchaseVoucher,
+        PurchasePass,
     },
     methods: {
+        purchaseVoucher: function () {
+            this.passTypeId = 0;
+            console.log('this.passTypeId = ' + this.passTypeId)
+            this.showHomeContent = false;
+            this.showPurchasePass = false;
+            this.showPurchaseVoucher = true;
+        },
+        purchasePass: function (passTypeId) {
+            this.passTypeId = passTypeId;
+            this.showHomeContent = false;
+            this.showPurchaseVoucher = false;
+            this.showPurchasePass = true;
+        },
         redirectToFAQ: function() {
             window.location.href = 'faq/'
         },
