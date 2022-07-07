9fa21d5b1c45c4c769a73a624cf3dbc7b898bf75
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-07 17:05:19 +0800

    More work on purchase voucher functionality

--- parkpasses/frontend/parkpasses/src/components/external/PurchaseVoucher.vue
+++ parkpasses/frontend/parkpasses/src/components/external/PurchaseVoucher.vue
@@ -9,12 +9,13 @@
         </p>
 
         <div>
+            <form>
             <div class="row g-3 align-items-center mb-2">
                 <div class="col-md-4">
                     <label for="amount" class="col-form-label">Voucher Amount</label>
                 </div>
                 <div class="col-auto">
-                    <input type="number" id="amount" name="amount" class="form-control" ref="amount" min="5" value="5" step="5" required="required" autofocus>
+                    <input type="number" id="amount" name="amount" v-model="voucher.amount" class="form-control" ref="amount" min="5" step="5" required="required" autofocus>
                 </div>
             </div>
             <div class="row g-3 align-items-center mb-2">
@@ -22,7 +23,7 @@
                     <label for="recipientName" class="col-form-label">Recipient Name</label>
                 </div>
                 <div class="col-auto">
-                    <input type="text" id="recipientName" name="recipientName" class="form-control" required="required">
+                    <input type="text" id="recipientName" name="recipientName" v-model="voucher.recipient_name" class="form-control" required="required">
                 </div>
             </div>
             <div class="row g-3 align-items-center mb-2">
@@ -30,7 +31,7 @@
                     <label for="recipientEmail" class="col-form-label">Recipient Email Address</label>
                 </div>
                 <div class="col-auto">
-                    <input type="email" id="recipientEmail" name="recipientEmail" class="form-control" required="required">
+                    <input type="email" id="recipientEmail" name="recipientEmail" v-model="voucher.recipient_email" class="form-control" required="required">
                 </div>
             </div>
             <div class="row g-3 align-items-center mb-2">
@@ -38,7 +39,8 @@
                     <label for="confirmRecipientEmail" class="col-form-label">Confirm Recipient Email Address</label>
                 </div>
                 <div class="col-auto">
-                    <input type="email" id="confirmRecipientEmail" name="confirmRecipientEmail" class="form-control" required="required">
+                    <input @change="validateConfirmRecipientEmail" type="email" id="confirmRecipientEmail" name="confirmRecipientEmail" v-model="confirm_recipient_email" class="form-control" required="required">
+                    <div v-if="errors.validateConfirmRecipientEmailError" class="col-auto alert alert-danger ml-2"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> {{errors.validateConfirmRecipientEmailError}}</div>
                 </div>
             </div>
             <div class="row g-3 align-items-center mb-2">
@@ -46,15 +48,18 @@
                     <label for="personalMessage" class="col-form-label">Message to Recipient</label>
                 </div>
                 <div class="col-auto">
-                    <textarea id="personalMessage" name="personalMessage" class="form-control" required="required"></textarea>
+                    <textarea id="personalMessage" name="personalMessage" v-model="voucher.personal_message" class="form-control" required="required"></textarea>
                 </div>
             </div>
             <div class="row g-3 align-items-center mb-2">
-                <div class="col-md-4">
+                <div class="col-md-4 align-top">
                     <label for="datetimeToEmail" class="col-form-label">Date to Send the Voucher</label>
                 </div>
                 <div class="col-auto">
-                    <input type="date" id="datetimeToEmail" name="datetimeToEmail" class="form-control" required="required">
+                    <input type="date" id="datetimeToEmail" name="datetimeToEmail" v-model="voucher.datetime_to_email" class="form-control" required="required" :min="startDate()" :max="endDate()">
+                </div>
+                <div class="col-auto">
+                    <i class="fa-solid fa-circle-question icon-help" data-bs-toggle="tooltip" data-bs-placement="right" title="Leave as today to have the voucher sent immediately"></i>
                 </div>
             </div>
             <div class="row g-3 align-items-center mb-2">
@@ -62,7 +67,7 @@
                     <label for="firstName" class="col-form-label">First Name</label>
                 </div>
                 <div class="col-auto">
-                    <input type="text" id="firstName" name="firstName" class="form-control" ref="firstName" required="required">
+                    <input type="text" id="firstName" name="firstName" v-model="voucher.purchaser_first_name" class="form-control" ref="firstName" required="required">
                 </div>
             </div>
             <div class="row g-3 align-items-center mb-2">
@@ -70,7 +75,7 @@
                     <label for="lastName" class="col-form-label">Last Name</label>
                 </div>
                 <div class="col-auto">
-                    <input type="text" id="lastName" name="lastName" class="form-control" required="required">
+                    <input type="text" id="lastName" name="lastName" v-model="voucher.purchaser_last_name" class="form-control" required="required">
                 </div>
             </div>
             <div class="row g-3 align-items-center mb-2">
@@ -78,7 +83,7 @@
                     <label for="email" class="col-form-label">Your Email Address</label>
                 </div>
                 <div class="col-auto">
-                    <input type="email" id="email" name="email" class="form-control" required="required">
+                    <input type="email" id="email" name="email" v-model="voucher.purchaser_email" class="form-control" required="required">
                 </div>
             </div>
             <div class="row g-3 align-items-center mb-2">
@@ -86,7 +91,7 @@
                     <label for="confirmEmail" class="col-form-label">Confirm Your Email</label>
                 </div>
                 <div class="col-auto">
-                    <input type="email" id="confirmEmail" name="confirmEmail" class="form-control" required="required">
+                    <input type="email" id="confirmEmail" name="confirmEmail" v-model="confirm_purchaser_email" class="form-control" required="required">
                 </div>
             </div>
             <div class="row g-3 align-items-center mb-2">
@@ -94,7 +99,7 @@
                     <label for="postcode" class="col-form-label">Your Postcode</label>
                 </div>
                 <div class="col-auto">
-                    <input type="text" id="postcode" name="postcode" class="form-control" pattern="[0-9]{4}" required="required">
+                    <input type="text" id="postcode" name="postcode" class="form-control" pattern="[0-9]{4}">
                 </div>
             </div>
             <div class="row g-3mb-2">
@@ -102,30 +107,80 @@
                     &nbsp;
                 </div>
                 <div class="col-auto">
-                    <button @click="submitForm" class="btn btn-primary px-5" type="submit">Pay</button>
+                    <button @click="submitForm" class="btn btn-primary px-5" type="button">Pay</button>
                 </div>
             </div>
+            </form>
         </div>
     </div>
 </template>
 
 <script>
+import { api_endpoints } from '@/utils/hooks'
 
 export default {
     name: "PurchaseVoucher",
     data: function () {
         return {
-            voucher: {},
+            voucher: {
+                amount: 5,
+                datetime_to_email: this.startDate()
+            },
+            confirm_recipient_email: '',
+            confirm_purchaser_email: '',
+            errors: {
+                validateConfirmRecipientEmailError: ""
+            },
         };
     },
     components: {
-
+        api_endpoints
     },
     computed: {
 
     },
     methods: {
-
+        startDate: function () {
+            const today = new Date();
+            return today.toISOString().split('T')[0];
+        },
+        endDate: function () {
+            let today = new Date();
+            today.setFullYear(today.getFullYear() + 1);
+            return today.toISOString().split('T')[0];
+        },
+        validateConfirmRecipientEmail: function () {
+            if(this.voucher.recipient_email===this.confirm_recipient_email){
+                this.errors.validateConfirmRecipientEmailError = '';
+                return true;
+            }
+            this.errors.validateConfirmRecipientEmailError = 'Must match recipient email address';
+            return false;
+        },
+        submitForm: function () {
+            let vm = this;
+            const requestOptions = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(vm.voucher)
+            };
+            fetch(api_endpoints.createVoucher, requestOptions)
+            .then(async response => {
+                const data = await response.json();
+                if (!response.ok) {
+                    const error = (data && data.message) || response.statusText;
+                    console.log(error)
+                    return Promise.reject(error);
+                }
+                vm.concessions = data.results
+            })
+            .catch(error => {
+                this.systemErrorMessage = "ERROR: Please try again in an hour.";
+                console.error("There was an error!", error);
+            });
+            console.log(this.voucher);
+            return false;
+        }
     },
     created: function () {
 
@@ -137,5 +192,7 @@ export default {
 </script>
 
 <style scoped>
-
+.icon-help{
+    color:blue;
+}
 </style>
