6e09f0204044ddefb2aed0ac89928dc5000cabc5
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-08 11:55:59 +0800

    Added import of style.scss

--- parkpasses/frontend/parkpasses/src/main.js
+++ parkpasses/frontend/parkpasses/src/main.js
@@ -1,5 +1,6 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+import '@/assets/sass/style.scss';
+
 import { createApp } from 'vue'
 import router from './router'
 import App from './App'
