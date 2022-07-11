cc7fe342be3684e9589c08c2d7c2b4574ddc848a
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-11 17:11:36 +0800

    Added CheckoutView.

---
+++ parkpasses/components/cart/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.views.generic import ListView
+
+from parkpasses.components.cart.models import Cart, CartItem
+from parkpasses.forms import LoginForm
+from parkpasses.helpers import is_internal
+
+
+class CheckoutView(LoginRequiredMixin, ListView):
+    template_name = "parkpasses/checkout.html"
+    model = CartItem
+
+    def get_queryset(self):
+        if self.request.session.get("cart_id", None):
+            cart = Cart.objects.get(id=self.request.session["cart_id"])
+            return CartItem.objects.filter(cart=cart)
+        else:
+            return CartItem.objects.none()
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            if is_internal(self.request):
+                return redirect("internal")
+        kwargs["form"] = LoginForm
+        return super().get(*args, **kwargs)
