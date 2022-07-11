7fdfb76f1893c18e0db0f836212c6661306f463f
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-11 17:07:35 +0800

    Added a checkout page.

---
+++ parkpasses/frontend/parkpasses/src/components/external/Checkout.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="container" id="shopHome">
+    <div class="row">
+
+      <div class="col-4">
+
+        Helpful Stuff will go here.
+
+      </div>
+
+      <div class="col">
+
+        <h1>Checkout</h1>
+
+        <div v-if="cartItems" class="accordion" id="accordionExample">
+            <CartItem v-for="cartItem in cartItems" :cartItem="cartItem" :key="cartItem.id" />
+
+        </div>
+        <div v-else>
+            There are no items in your cart.
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { api_endpoints, helpers } from '@/utils/hooks'
+import CartItem from '@/components/external/CartItem.vue'
+
+export default {
+    name: "Checkout",
+    data: function () {
+        return {
+            cartItems: [],
+        };
+    },
+    components: {
+        api_endpoints,
+        helpers,
+        CartItem
+    },
+    computed: {
+        totalPrice() {
+            if(0==this.cartItems.length){
+                return '$0.00'
+            } else if(1==this.cartItems.length) {
+                return this.cartItems[0].amount
+            } else {
+                const total = this.cartItems.reduce((accumulator, object) => {
+                    return accumulator + object.amount;
+                }, 0);
+            }
+        }
+    },
+    methods: {
+        functionName: function () {
+
+        },
+        fetchCartItems: function () {
+            let vm = this;
+            fetch(api_endpoints.checkout)
+            .then(async response => {
+                const data = await response.json();
+                if (!response.ok) {
+                    const error = (data && data.message) || response.statusText;
+                    console.log(error);
+                    return Promise.reject(error);
+                }
+                // Do something after adding the voucher to the database and the users cart
+                vm.cartItems = data
+            })
+            .catch(error => {
+                vm.systemErrorMessage = "ERROR: Please try again in an hour.";
+                console.error("There was an error!", error);
+            });
+            console.log(this.cartItems);
+        }
+    },
+    created: function () {
+        this.fetchCartItems();
+    },
+    mounted: function () {
+
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
