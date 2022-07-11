842b8af4ba97935158512583ca2ba97556f60beb
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-11 17:10:35 +0800

    Added checkout url to patterns.

--- parkpasses/components/cart/urls.py
+++ parkpasses/components/cart/urls.py
@@ -1,10 +1,15 @@
+from django.conf.urls import url
 from rest_framework import routers
 
-from parkpasses.components.cart.api import CartItemViewSet, CartViewSet
+from parkpasses.components.cart.api import CartItemViewSet, CartViewSet, CheckoutView
 
 router = routers.SimpleRouter()
 
 router.register(r"carts", CartViewSet, basename="carts")
 router.register(r"cart-items", CartItemViewSet, basename="cart-items")
 
-urlpatterns = router.urls
+urlpatterns = [
+    url(r"checkout", CheckoutView.as_view()),
+]
+
+urlpatterns = router.urls + urlpatterns
