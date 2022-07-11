0c5c0fd4b762f742a4d1ff41296a30d13ac6fc05
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-11 17:15:36 +0800

    Added checkout route.

--- parkpasses/frontend/parkpasses/src/components/external/routes/index.js
+++ parkpasses/frontend/parkpasses/src/components/external/routes/index.js
@@ -2,6 +2,7 @@ import { RouterView } from 'vue-router'
 import ShopHome from '@/components/external/ShopHome.vue'
 import PurchaseVoucher from '@/components/external/PurchaseVoucher.vue'
 import PurchasePass from '@/components/external/PurchasePass.vue'
+import Checkout from '@/components/external/Checkout.vue'
 import ExternalDashboard from '@/components/external/Dashboard.vue'
 export default
 /*[{
@@ -35,6 +36,11 @@ export default
             path: '/purchase-pass/:passTypeId',
             component: PurchasePass,
             name: 'purchase-pass'
+        },
+        {
+            path: '/checkout',
+            component: Checkout,
+            name: 'checkout'
         },
     ]
 }
