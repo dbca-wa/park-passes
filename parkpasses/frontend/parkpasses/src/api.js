6c1a940280195fded902d150a3200002fd5ddd5c
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-11 17:14:36 +0800

    Added end-points for createVoucher and checkout.

--- parkpasses/frontend/parkpasses/src/api.js
+++ parkpasses/frontend/parkpasses/src/api.js
@@ -8,12 +8,12 @@ module.exports = {
         return                          `/api/concessions/concessions/${id}/?format=json`;
     },
 
-    /* ========================= Passes ================================================*/
+    /* ========================= Passes =================================================*/
 
     passesPaginatedList:                '/api/passes/passes/',
     passProcessingStatusesDistinct:     "/api/passes/pass-processing-statuses-distinct?format=json",
 
-    /* ========================= Pass Types ============================================*/
+    /* ========================= Pass Types =============================================*/
 
     passTypesDistinct:                  "/api/passes/pass-types-distinct?format=json",
     passTypes:                          "/api/passes/pass-types?format=json",
@@ -21,20 +21,25 @@ module.exports = {
         return                          `/api/passes/pass-types/${id}/?format=json`;
     },
 
-    /* ========================= Pass Options ============================================*/
+    /* ========================= Pass Options ===========================================*/
 
     passOptions: function (id) {
         return                          `/api/passes/pass-options-by-pass-type-id?pass_type_id=${id}`;
     },
 
-    /* ========================= Discount Codes ============================================*/
+    /* ========================= Discount Codes =========================================*/
     isDiscountCodeValid: function (id) {
         return                          `to be implimented`;
     },
 
-    /* ========================= Vouchers ============================================*/
+    /* ========================= Vouchers ===============================================*/
     isVoucherValid: function (id) {
         return                          `to be implimented`;
     },
-    createVoucher:                      "/api/vouchers/external/vouchers/?format=json",
+    createVoucher:                      "/api/vouchers/external/vouchers/",
+
+    /* ========================= Cart Items =============================================*/
+
+    checkout:                          "/api/cart/checkout/?format=json",
+
 }
