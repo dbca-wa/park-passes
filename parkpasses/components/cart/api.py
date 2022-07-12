8cfd7cf62110f5d34f3b376b54fd87ff2e48c956
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-12 16:59:41 +0800

    Further implimentation of checkout view and started on success view.

--- parkpasses/components/cart/api.py
+++ parkpasses/components/cart/api.py
@@ -1,6 +1,8 @@
 import logging
 
+from ledger_api_client.ledger_models import EmailUserRO as EmailUser
 from rest_framework import viewsets
+from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -51,22 +53,36 @@ class CartItemViewSet(viewsets.ModelViewSet):
             return Cart.objects.none()
 
 
-class CheckoutView(APIView):
+class CheckoutView(GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, format=None):
-        if self.request.session.get("cart_id", None):
-            cart_id = self.request.session["cart_id"]
+        if request.session.get("cart_id", None):
+            cart_id = request.session["cart_id"]
             cart = Cart.objects.get(id=cart_id)
             logger.debug("cart = " + str(cart))
             logger.debug("cart.items = " + str(cart.items))
+            purchaser = EmailUser.objects.get(id=cart.user)
             cart_items = CartItem.objects.filter(cart=cart)
             objects = []
             for cart_item in cart_items:
                 item = CartUtils.get_serialized_object_by_id_and_content_type(
                     cart_item.object_id, cart_item.content_type.id
                 )
+                item.purchaser_email = purchaser.email
+                item.purchaser_first_name = purchaser.first_name
+                item.purchaser_last_name = purchaser.last_name
                 objects.append(item)
             return Response(objects)
         else:
             return Response([])
+
+
+class SuccessView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        if request.session.get("cart_id", None):
+            cart_id = request.session["cart_id"]
+            cart = Cart.objects.get(id=cart_id)
+            cart.create_order(True)
