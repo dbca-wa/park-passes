e734dc0b147b3006cb60114355e11f380e3b4723
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-13 11:27:31 +0800

    Added LedgerCheckoutView adn SuccessView.

--- parkpasses/components/cart/api.py
+++ parkpasses/components/cart/api.py
@@ -1,6 +1,11 @@
 import logging
+import pprint
 
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect
 from ledger_api_client.ledger_models import EmailUserRO as EmailUser
+from ledger_api_client.utils import create_basket_session, create_checkout_session
 from rest_framework import viewsets
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
@@ -10,6 +15,7 @@ from rest_framework.views import APIView
 from parkpasses.components.cart.models import Cart, CartItem
 from parkpasses.components.cart.serializers import CartItemSerializer, CartSerializer
 from parkpasses.components.cart.utils import CartUtils
+from parkpasses.components.orders.serializers import OrderListItemSerializer
 from parkpasses.helpers import is_internal
 
 logger = logging.getLogger(__name__)
@@ -78,11 +84,57 @@ class CheckoutView(GenericAPIView):
             return Response([])
 
 
+class LedgerCheckoutView(APIView):
+    def get_ledger_order_lines(self):
+        ledger_order_lines = []
+        line_status = settings.PARKPASSES_LEDGER_DEFAULT_LINE_STATUS
+        order, order_items = self.create_order()
+        for order_item in order_items:
+            ledger_order_line = {
+                "ledger_description": order_item.description,
+                "quantity": 1,
+                "price_incl_tax": order_item.amount,
+                "oracle_code": CartUtils.get_oracle_code(),
+                "line_status": line_status,
+            }
+            ledger_order_lines.append(ledger_order_line)
+            logger.debug(pprint.pformat(ledger_order_line))
+        return ledger_order_lines
+
+    def post(self, request, format=None):
+        ledger_order_lines = self.get_ledger_order_lines()
+        is_no_payment = self.request.data["no_payment"]
+        basket_parameters = CartUtils.get_basket_parameters(
+            ledger_order_lines, is_no_payment
+        )
+        create_basket_session(request, self.user, basket_parameters)
+        checkouthash = request.session.get("checkouthash", "")
+        checkout_parameters = CartUtils.get_checkout_parameters(
+            request, self, checkouthash
+        )
+        create_checkout_session(request, checkout_parameters)
+        return redirect("ledgergw-payment-details")
+
+
 class SuccessView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, format=None):
-        if request.session.get("cart_id", None):
-            cart_id = request.session["cart_id"]
-            cart = Cart.objects.get(id=cart_id)
-            cart.create_order(True)
+        uuid = self.request.query_params.get("uuid")
+        cart_id = self.request.query_params.get("cart_id")
+        if uuid and cart_id:
+            try:
+                cart = Cart.objects.get(id=cart_id, uuid=uuid)
+            except ObjectDoesNotExist:
+                logger.warning(
+                    "Client has requested cart success view for cart with id:\
+                     {} and uuid: {} that doesn't exist.".format(
+                        cart_id, uuid
+                    )
+                )
+                return Response([])
+            # Create the order and order lines, save them to the database and then delete the cart.
+            order, order_items = cart.create_order(True)
+            serializer = OrderListItemSerializer(order)
+
+        return Response(serializer.data)
