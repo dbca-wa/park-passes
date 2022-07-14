e22f6793f9fe42eec62eec2aae94105dcea4117e
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-14 15:42:50 +0800

    Added bootstrap front-end form validation.

--- parkpasses/frontend/parkpasses/src/components/external/PurchaseVoucher.vue
+++ parkpasses/frontend/parkpasses/src/components/external/PurchaseVoucher.vue
@@ -9,49 +9,63 @@
         </p>
 
         <div>
-            <form>
-            <div class="row g-3 align-items-center mb-2">
+            <form @submit.prevent="validateForm" @keydown.enter="$event.preventDefault()" class="needs-validation" novalidate>
+            <div class="row g-1 align-top mb-2">
                 <div class="col-md-4">
                     <label for="amount" class="col-form-label">Voucher Amount</label>
                 </div>
                 <div class="col-auto">
                     <input type="number" id="amount" name="amount" v-model="voucher.amount" class="form-control" ref="amount" min="5" step="5" required="required" autofocus>
+                    <div class="invalid-feedback">
+                        Please enter a valid voucher amount.
+                    </div>
                 </div>
             </div>
-            <div class="row g-3 mb-2">
+            <div class="row g-1 align-top mb-2">
                 <div class="col-md-4 mt-auto align-top">
                     <label for="recipientName" class="col-form-label">Recipient Name</label>
                 </div>
                 <div class="col-auto">
                     <input type="text" id="recipientName" name="recipientName" v-model="voucher.recipient_name" class="form-control" required="required">
+                    <div class="invalid-feedback">
+                        Please enter the recipient's name.
+                    </div>
                 </div>
             </div>
-            <div class="row g-3 align-items-center mb-2">
+            <div class="row g-1 align-top mb-2">
                 <div class="col-md-4">
                     <label for="recipientEmail" class="col-form-label">Recipient Email Address</label>
                 </div>
                 <div class="col-auto">
                     <input type="email" id="recipientEmail" name="recipientEmail" v-model="voucher.recipient_email" class="form-control" required="required">
+                    <div class="invalid-feedback">
+                        Please enter a valid email for the recipient.
+                    </div>
                 </div>
             </div>
-            <div class="row g-3 align-items-center mb-2">
+            <div class="row g-1 align-top mb-2">
                 <div class="col-md-4">
                     <label for="confirmRecipientEmail" class="col-form-label">Confirm Recipient Email Address</label>
                 </div>
                 <div class="col-auto">
-                    <input @change="validateConfirmRecipientEmail" type="email" id="confirmRecipientEmail" name="confirmRecipientEmail" v-model="confirm_recipient_email" class="form-control" required="required">
-                    <div v-if="errors.validateConfirmRecipientEmailError" class="col-auto alert alert-danger ml-2"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> {{errors.validateConfirmRecipientEmailError}}</div>
+                    <input @change="validateConfirmRecipientEmail" type="email" id="confirmRecipientEmail" name="confirmRecipientEmail" v-model="confirmRecipientEmail" ref="confirmRecipientEmail" class="form-control" required="required">
+                    <div class="invalid-feedback">
+                        Please make sure this email matches the recipient's email.
+                    </div>
                 </div>
             </div>
-            <div class="row g-3 align-top mb-2">
+            <div class="row g-1 align-top mb-2">
                 <div class="col-md-4">
                     <label for="personalMessage" class="col-form-label">Message to Recipient</label>
                 </div>
                 <div class="col-auto">
                     <textarea id="personalMessage" name="personalMessage" v-model="voucher.personal_message" class="form-control personalMessage" required="required"></textarea>
+                    <div class="invalid-feedback">
+                        Please enter a personal message for the recipient.
+                    </div>
                 </div>
             </div>
-            <div class="row g-3 align-items-center mb-2">
+            <div class="row g-1 align-top mb-2">
                 <div class="col-md-4 align-top">
                     <label for="datetimeToEmail" class="col-form-label">Date to Send the Voucher</label>
                 </div>
@@ -59,11 +73,11 @@
                     <input type="date" id="datetimeToEmail" name="datetimeToEmail" v-model="voucher.datetimeToEmail" class="form-control" required="required" :min="startDate()" :max="endDate()">
                 </div>
                 <div class="col-auto">
-                    <i class="fa-solid fa-circle-question org-icon-help-primary" data-bs-toggle="tooltip" data-bs-placement="right" title="Leave as today to have the voucher sent immediately"></i>
+                    <i class="fa-solid fa-circle-question org-icon-primary" data-bs-toggle="tooltip" data-bs-placement="right" title="Leave as today to have the voucher sent immediately"></i>
                 </div>
             </div>
             <!--
-            <div class="row g-3 align-items-center mb-2">
+            <div class="row g-1 align-top mb-2">
                 <div class="col-md-4">
                     <label for="firstName" class="col-form-label">Your First Name</label>
                 </div>
@@ -71,7 +85,7 @@
                     <input type="text" id="firstName" name="firstName" v-model="voucher.purchaser_first_name" class="form-control" ref="firstName" required="required">
                 </div>
             </div>
-            <div class="row g-3 align-items-center mb-2">
+            <div class="row g-1 align-top mb-2">
                 <div class="col-md-4">
                     <label for="lastName" class="col-form-label">Your Last Name</label>
                 </div>
@@ -80,7 +94,7 @@
                 </div>
             </div>
 
-            <div class="row g-3 align-items-center mb-2">
+            <div class="row g-1 align-top mb-2">
                 <div class="col-md-4">
                     <label for="email" class="col-form-label">Your Email Address</label>
                 </div>
@@ -88,7 +102,7 @@
                     <input type="email" id="email" name="email" v-model="voucher.purchaser_email" class="form-control" required="required">
                 </div>
             </div>
-            <div class="row g-3 align-items-center mb-2">
+            <div class="row g-1 align-top mb-2">
                 <div class="col-md-4">
                     <label for="confirmEmail" class="col-form-label">Confirm Your Email</label>
                 </div>
@@ -96,7 +110,7 @@
                     <input type="email" id="confirmEmail" name="confirmEmail" v-model="confirm_purchaser_email" class="form-control" required="required">
                 </div>
             </div>
-            <div class="row g-3 align-items-center mb-2">
+            <div class="row g-1 align-top mb-2">
                 <div class="col-md-4">
                     <label for="postcode" class="col-form-label">Your Postcode</label>
                 </div>
@@ -110,7 +124,7 @@
                     &nbsp;
                 </div>
                 <div class="col-auto">
-                    <button @click="submitForm" class="btn licensing-btn-primary px-5" type="button">Next</button>
+                    <button class="btn licensing-btn-primary px-5" type="submit">Next</button>
                 </div>
             </div>
             </form>
@@ -129,8 +143,7 @@ export default {
                 amount: 5,
                 datetimeToEmail: this.startDate()
             },
-            confirm_recipient_email: '',
-            confirm_purchaser_email: '',
+            confirmRecipientEmail: '',
             errors: {
                 validateConfirmRecipientEmailError: ""
             },
@@ -153,13 +166,13 @@ export default {
             today.setFullYear(today.getFullYear() + 1);
             return today.toISOString().split('T')[0];
         },
-        validateConfirmRecipientEmail: function () {
-            if(this.voucher.recipient_email===this.confirm_recipient_email){
-                this.errors.validateConfirmRecipientEmailError = '';
-                return true;
+        validateConfirmEmail: function () {
+            if(this.confirmRecipientEmail!=this.voucher.recipient_email){
+                this.$refs.confirmRecipientEmail.setCustomValidity("Invalid field.");
+                return false;
+            } else {
+                this.$refs.confirmRecipientEmail.setCustomValidity("");
             }
-            this.errors.validateConfirmRecipientEmailError = 'Must match recipient email address';
-            return false;
         },
         submitForm: function () {
             let vm = this;
@@ -187,6 +200,24 @@ export default {
             });
             console.log(this.voucher);
             return false;
+        },
+        validateForm: function () {
+            let vm = this;
+            var forms = document.querySelectorAll('.needs-validation');
+
+            Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                if(form.checkValidity()){
+                    vm.submitForm();
+                } else {
+                    form.classList.add('was-validated');
+                    $(".invalid-feedback:visible:first").siblings('input').focus();
+                }
+
+            });
+
+            console.log(this.voucher);
+            return false;
         }
     },
     created: function () {
@@ -199,8 +230,10 @@ export default {
 </script>
 
 <style scoped>
+    .form-control{
+        width:350px;
+    }
     .personalMessage{
-        width:275px;
         height:200px;
     }
 </style>
