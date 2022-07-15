30c2b326342a9711afc1b57743040724a3c00752
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-15 09:54:54 +0800

    Enabled editing of help text.

--- parkpasses/components/help/admin.py
+++ parkpasses/components/help/admin.py
@@ -6,14 +6,6 @@ from parkpasses.components.help.models import HelpText
 class HelpTextAdmin(admin.ModelAdmin):
     model = HelpText
     list_display = ("label", "version")
-    readonly_fields = [
-        "content",
-    ]
-
-    def get_readonly_fields(self, request, obj=None):
-        if obj:  # Editing
-            return self.readonly_fields
-        return ()
 
 
 admin.site.register(HelpText, HelpTextAdmin)
