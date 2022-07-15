e94e2259bfdf084371ab930802fbfdc7e1d412aa
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-15 17:07:22 +0800

    Added more user data to end point.

--- parkpasses/components/users/api.py
+++ parkpasses/components/users/api.py
@@ -1,5 +1,6 @@
 import logging
 
+from ledger_api_client.ledger_models import EmailUserRO as EmailUser
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,20 +14,22 @@ class UserDataView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get(self, request, format=None):
+        user_data = {}
         # retailer_group = ''
+        user_data["isAuthenticated"] = request.user.is_authenticated
+        if user_data["isAuthenticated"]:
+            emailuser = EmailUser.objects.get(id=request.user.id)
+            user_data["user"] = {}
+            user_data["user"]["email"] = emailuser.email
+            user_data["user"]["first_name"] = emailuser.first_name
+            user_data["user"]["last_name"] = emailuser.last_name
         if is_internal(request):
-            authorisation_level = "internal"
+            user_data["authorisationLevel"] = "internal"
         elif is_retailer(request):
-            authorisation_level = "retailer"
+            user_data["authorisationLevel"] = "retailer"
             # Todo: At some stage we will need to add the retailer group
             # retailer_group =
         else:
-            authorisation_level = "external"
+            user_data["authorisationLevel"] = "external"
 
-        return Response(
-            {
-                "isAuthenticated": request.user.is_authenticated,
-                "authorisationLevel": authorisation_level,
-                # "authorisationLevel": authorisation_level,
-            }
-        )
+        return Response(user_data)
