9ba2e22fa9d0cc5f57ca63a31cdeaeb95b3fd91d
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-26 16:22:06 +0800

    Added ExternalParkSerializer and ExternalParkGroupSerializer.

--- parkpasses/components/parks/serializers.py
+++ parkpasses/components/parks/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from parkpasses.components.parks.models import LGA, Park, Postcode
+from parkpasses.components.parks.models import LGA, Park, ParkGroup, Postcode
 
 
 class PostcodeSerializer(serializers.ModelSerializer):
@@ -15,7 +15,25 @@ class ParkSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class ExternalParkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Park
+        fields = ["id", "name"]
+
+
 class LGASerializer(serializers.ModelSerializer):
     class Meta:
         model = LGA
         fields = "__all__"
+
+
+class ExternalParkGroupSerializer(serializers.ModelSerializer):
+    parks = ExternalParkSerializer(many=True)
+
+    class Meta:
+        model = ParkGroup
+        fields = [
+            "id",
+            "name",
+            "parks",
+        ]
