3407fa46f27808e286881bc65a037e97dbcda8f5
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-26 16:22:45 +0800

    Added urls for validating postcode and listing park groups for a specific postcode.

--- parkpasses/components/parks/urls.py
+++ parkpasses/components/parks/urls.py
@@ -1,10 +1,22 @@
+from django.conf.urls import url
 from rest_framework import routers
 
-from parkpasses.components.parks.api import LGAViewSet, ParkViewSet, PostcodeViewSet
+from parkpasses.components.parks.api import (
+    LGAViewSet,
+    ParkGroupsForPostcodeView,
+    ParkViewSet,
+    PostcodeViewSet,
+    ValidatePostcodeView,
+)
 
 router = routers.SimpleRouter()
 router.register(r"postcodes", PostcodeViewSet, basename="postcodes")
 router.register(r"parks", ParkViewSet, basename="parks")
 router.register(r"lgas", LGAViewSet, basename="lgas")
 
-urlpatterns = router.urls
+urlpatterns = [
+    url(r"validate-postcode", ValidatePostcodeView.as_view()),
+    url(r"park-groups-for-postcode", ParkGroupsForPostcodeView.as_view()),
+]
+
+urlpatterns += router.urls
