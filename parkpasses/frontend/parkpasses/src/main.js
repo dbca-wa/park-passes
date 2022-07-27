bd1de5620db4e605ef8fb732f40dd56c78472f8f
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-27 11:30:21 +0800

    Added pinia back in.

--- parkpasses/frontend/parkpasses/src/main.js
+++ parkpasses/frontend/parkpasses/src/main.js
@@ -6,9 +6,9 @@ import App from './App'
 import helpers from '@/utils/helpers'
 import { useStore } from '@/stores/state'
 import CKEditor from '@ckeditor/ckeditor5-vue';
-import('@/../node_modules/@fortawesome/fontawesome-free/css/all.min.css')
-import('@/../node_modules/select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.css')
-import('@/../node_modules/datatables.net-bs5/css/dataTables.bootstrap5.min.css')
+require('@/../node_modules/@fortawesome/fontawesome-free/css/all.min.css')
+require('@/../node_modules/select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.css')
+require('@/../node_modules/datatables.net-bs5/css/dataTables.bootstrap5.min.css')
 
 // Add CSRF Token to every request
 const customHeaders = new Headers({
