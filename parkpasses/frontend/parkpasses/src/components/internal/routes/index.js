7c61ed3257b9396fb0dedd1c682e719cfd77d0db
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-07-29 09:07:26 +0800

    Added routes for internal vouchers and discount codes.

--- parkpasses/frontend/parkpasses/src/components/internal/routes/index.js
+++ parkpasses/frontend/parkpasses/src/components/internal/routes/index.js
@@ -1,6 +1,8 @@
 
 import { RouterView } from 'vue-router'
 import InternalDashboard from '@/components/internal/Dashboard.vue'
+import InternalVouchers from '@/components/internal/Vouchers.vue'
+import InternalDiscountCodes from '@/components/internal/DiscountCodes.vue'
 
 export default
 {
@@ -10,6 +12,16 @@ export default
         {
             path: '/internal',
             component: InternalDashboard
+        },
+        {
+            path: '/internal/vouchers',
+            component: InternalVouchers,
+            name: 'internal-vouchers'
+        },
+        {
+            path: '/internal/discount-codes',
+            component: InternalDiscountCodes,
+            name: 'innteral-discount-codes'
         },
     ]
 }
