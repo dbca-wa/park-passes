f368ef14229cfaf4fa8f53281138a1a57f7893ff
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-08-02 11:59:50 +0800

    Added MultipleDefaultPricingWindowsExist and NoDefaultPricingWindowExists exceptions.

--- parkpasses/components/passes/exceptions.py
+++ parkpasses/components/passes/exceptions.py
@@ -6,3 +6,11 @@ logger = logging.getLogger(__name__)
 
 class PassTemplateDoesNotExist(Exception):
     """The exception to be raised if there is no pass template"""
+
+
+class MultipleDefaultPricingWindowsExist(Exception):
+    """The exception to be thrown if there is more than one default pricing window for pass type"""
+
+
+class NoDefaultPricingWindowExists(Exception):
+    """The exception to be thrown if there is no default pricing window for pass type"""
