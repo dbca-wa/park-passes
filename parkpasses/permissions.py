79a82d4e1a35e9e27d6d679d79c4d0a6a49dbd11
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-08-05 14:56:46 +0800

    Added IsRetailer permission.

--- parkpasses/permissions.py
+++ parkpasses/permissions.py
@@ -1,8 +1,13 @@
 from rest_framework.permissions import BasePermission
 
-from parkpasses.helpers import is_internal
+from parkpasses.helpers import is_internal, is_retailer
 
 
 class IsInternal(BasePermission):
     def has_permission(self, request, view):
         return is_internal(request)
+
+
+class IsRetailer(BasePermission):
+    def has_permission(self, request, view):
+        return is_retailer(request)
