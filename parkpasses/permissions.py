f68865a5f3e935d7759977f2534395c41bc3ecee
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-08-09 12:26:04 +0800

    Added IsInternalOrReadOnly permission.

--- parkpasses/permissions.py
+++ parkpasses/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 from parkpasses.helpers import is_internal, is_retailer
 
@@ -6,6 +6,13 @@ from parkpasses.helpers import is_internal, is_retailer
 class IsInternal(BasePermission):
     def has_permission(self, request, view):
         return is_internal(request)
+
+
+class IsInternalOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS or request.user and is_internal(request)
+        )
 
 
 class IsRetailer(BasePermission):
