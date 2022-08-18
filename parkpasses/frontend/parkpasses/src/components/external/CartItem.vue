ae0235e757640aa656771d56bafab1443ac84b8b
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-08-18 09:24:55 +0800

    Added deleteCartItem method. Changed template to use cards with accordian collapse so I could have delete cart link clicked without accordian collapsing. Added some custom styles.

--- parkpasses/frontend/parkpasses/src/components/external/CartItem.vue
+++ parkpasses/frontend/parkpasses/src/components/external/CartItem.vue
@@ -1,12 +1,15 @@
 <template>
-    <div v-if="cartItem.hasOwnProperty('voucher_number')" class="accordion-item g-2">
-        <h2 class="accordion-header" :id="$.vnode.key">
-            <button class="accordion-button checkout-item-btn" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapse' + $.vnode.key" aria-expanded="true" :aria-controls="'collapse' + $.vnode.key">
-                <span class="item-type">Park Pass Voucher: {{cartItem.voucher_number}}</span> <span class="item-amount">${{cartItem.amount}}</span>
-            </button>
-        </h2>
-        <div id="collapseOne" class="accordion-collapse collapse show" :aria-labelledby="$.vnode.key">
-            <div class="accordion-body">
+
+    <div v-if="cartItem.hasOwnProperty('voucher_number')" class="card mb-1" :id="cartItem.cart_item_id">
+        <div class="card-header checkout-item-header">
+            <span class="item-type">Park Pass Voucher: {{cartItem.voucher_number}} </span>
+            <a class="accordian-header-note text-secondary" data-bs-toggle="collapse" :href="'#collapse' + $.vnode.key" role="button" aria-expanded="false" :aria-controls="'collapse' + $.vnode.key">Click to show details</a>
+            <span class="item-amount">${{cartItem.amount}}</span>
+            <span class="delete-button"><i @click="deleteCartItem($event, cartItem.cart_item_id)" class="fa fa-trash org-primary" aria-hidden="true"></i></span>
+        </div>
+
+        <div :id="'collapse' + $.vnode.key" class="collapse" aria-labelledby="headingOne" data-parent="#checkoutAccordion">
+            <div class="card-body">
                 <table class="table">
                     <tr><th>Voucher Code</th><td>{{cartItem.code}}</td></tr>
                     <tr><th>Recipient Name</th><td>{{cartItem.recipient_name}}</td></tr>
@@ -21,29 +24,32 @@
             </div>
         </div>
     </div>
-    <div v-if="cartItem.hasOwnProperty('pass_number')" class="accordion-item">
-        <h2 class="accordion-header" :id="$.vnode.key">
-            <button class="accordion-button checkout-item-btn" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapse' + $.vnode.key" aria-expanded="true" :aria-controls="'collapse' + $.vnode.key">
-                <span class="item-type">Park Pass: {{cartItem.pass_number}}</span> <span class="item-amount">${{cartItem.price}}</span>
-            </button>
-        </h2>
-        <div :id="'collapse' + $.vnode.key" class="accordion-collapse collapse show" :aria-labelledby="$.vnode.key">
-            <div class="accordion-body">
+
+    <div v-if="cartItem.hasOwnProperty('pass_number')" class="card" :id="cartItem.cart_item_id">
+        <div class="card-header checkout-item-header">
+            <span class="item-type">Park Pass: {{cartItem.pass_number}}</span>
+            <a class="accordian-header-note text-secondary" data-bs-toggle="collapse" :href="'#collapse' + $.vnode.key" role="button" aria-expanded="false" :aria-controls="'collapse' + $.vnode.key">Click to show details</a>
+            <span class="item-amount">${{cartItem.price}}</span>
+            <span class="delete-button"><i @click="deleteCartItem($event, cartItem.cart_item_id)" class="fa fa-trash org-primary" aria-hidden="true"></i></span>
+        </div>
+
+        <div :id="'collapse' + $.vnode.key" class="collapse" aria-labelledby="headingOne" data-parent="#checkoutAccordion">
+            <div class="card-body">
                 <table class="table">
-                    <tr><th>Pass Type</th><td>{{cartItem.pass_type}}</td></tr>
-                    <tr><th>Duration</th><td>{{cartItem.duration}}</td></tr>
-                    <tr><th>Pass Start Date</th><td>{{formatDate(cartItem.datetime_start)}}</td></tr>
-                    <tr><th>Pass Expiry Date</th><td>{{formatDate(cartItem.datetime_expiry)}}</td></tr>
-                    <tr v-if="cartItem.vehicle_registration_1"><th>Vehicle Registraion <span v-if="cartItem.vehicle_registration_2">1</span></th><td>{{cartItem.vehicle_registration_1}}</td></tr>
-                    <tr v-if="cartItem.vehicle_registration_2"><th>Vehicle Registraion <span v-if="cartItem.vehicle_registration_1">2</span></th><td>{{cartItem.vehicle_registration_2}}</td></tr>
-                    <tr><th>Your First Name</th><td>{{cartItem.first_name}}</td></tr>
-                    <tr><th>Your Last Name</th><td>{{cartItem.last_name}}</td></tr>
-                    <tr><th>Your Email</th><td>{{cartItem.email}}</td></tr>
-                    <tr><th>Price</th><td>${{cartItem.price}}</td></tr>
+                    <tr><th>Voucher Code</th><td>{{cartItem.code}}</td></tr>
+                    <tr><th>Recipient Name</th><td>{{cartItem.recipient_name}}</td></tr>
+                    <tr><th>Recipient Email</th><td>{{cartItem.recipient_email}}</td></tr>
+                    <tr><th>Date to Email</th><td>{{formatDate(cartItem.datetime_to_email)}}</td></tr>
+                    <tr><th>Personal Message</th><td>{{cartItem.personal_message}}</td></tr>
+                    <tr><th>Your First Name</th><td>{{cartItem.purchaser.first_name}}</td></tr>
+                    <tr><th>Your Last Name</th><td>{{cartItem.purchaser.last_name}}</td></tr>
+                    <tr><th>Your Email</th><td>{{cartItem.purchaser.email}}</td></tr>
+                    <tr><th>Amount</th><td>${{cartItem.amount}}</td></tr>
                 </table>
             </div>
         </div>
     </div>
+
 </template>
 
 <script>
@@ -54,6 +60,7 @@ export default {
     props: {
         cartItem: {},
     },
+    emits: ["deleteCartItem"],
     data: function () {
         return {
 
@@ -70,9 +77,28 @@ export default {
         formatDate(dateString) {
             const date = new Date(dateString);
             const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-                // Then specify how you want your dates to be formatted
             return date.toLocaleDateString('en-AU', options);
         },
+        deleteCartItem: function(event, cart_item_id) {
+            event.preventDefault();
+            event.stopPropagation();
+            console.log('Deleting cart item with id = ' + cart_item_id);
+            let vm = this;
+            fetch(apiEndpoints.deleteCartItem(cart_item_id), {method: "DELETE"})
+            .then(async response => {
+                console.log('response = ' + JSON.stringify(response))
+                $(`#${cart_item_id}`).fadeOut(400, function(){
+                    $(this).remove();
+                    vm.$emit("deleteCartItem", cart_item_id);
+                });
+            })
+            .catch(error => {
+                vm.systemErrorMessage = "ERROR: Please try again in an hour.";
+                console.error("There was an error!", error);
+            }).finally(() => {
+                vm.loading = false;
+            });
+        }
     },
     created: function () {
 
@@ -84,12 +110,23 @@ export default {
 </script>
 
 <style scoped>
-    button.checkout-item-btn {
-    /* create a grid */
-    display: grid;
-    /* create colums. 1fr means use available space */
-    grid-template-columns: 1fr max-content max-content;
-    align-items: center;
-    grid-gap: 10px;
+    .checkout-item-header {
+        /* create a grid */
+        display: grid;
+        /* create colums. 1fr means use available space */
+        grid-template-columns: max-content 1fr max-content max-content max-content;
+         grid-auto-flow: column;
+        align-items: center;
+        justify-content: center;
+        grid-gap: 10px;
+    }
+
+    .delete-button {
+        cursor:pointer;
+    }
+
+    .accordian-header-note {
+        font-size:0.9em;
+        text-decoration:none;
     }
 </style>
