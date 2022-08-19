a962a7920f9b2afdc51bec1cb3bc2f33a9031c43
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-08-19 09:38:14 +0800

    Vue component for checkout success page.

---
+++ parkpasses/frontend/parkpasses/src/components/external/CheckoutSuccess.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container" id="shopHome">
+        <div class="row">
+            <div class="col">
+                <h1>Checkout Success</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 p-3">
+                <p>You're order has been processed successfully. Yay!</p>
+
+                <p>Say some more stuff here.</p>
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+import { apiEndpoints } from '@/utils/hooks'
+import Loader from '@/utils/vue/Loader.vue'
+
+export default {
+    name: "FAQs",
+    data: function () {
+        return {
+            faqs: null
+        };
+    },
+    components: {
+        Loader
+    },
+    methods: {
+
+    },
+    created: function() {
+
+}
+};
+</script>
+
+<style scoped>
+
+</style>
