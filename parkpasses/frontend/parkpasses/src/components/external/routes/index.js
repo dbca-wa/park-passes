a7b0a4a87cef78ebbe51e726b7f777ab22af8c95
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-08-23 15:42:41 +0800

    Added routes for Help, Contact, YourParkPasses, YourOrders and YourVouchers.

--- parkpasses/frontend/parkpasses/src/components/external/routes/index.js
+++ parkpasses/frontend/parkpasses/src/components/external/routes/index.js
@@ -1,6 +1,11 @@
 import { RouterView } from 'vue-router'
 import ShopHome from '@/components/external/ShopHome.vue'
 import FAQs from '@/components/external/FAQs.vue'
+import Help from '@/components/external/Help.vue'
+import Contact from '@/components/external/Contact.vue'
+import YourParkPasses from '@/components/external/YourParkPasses.vue'
+import YourOrders from '@/components/external/YourOrders.vue'
+import YourVouchers from '@/components/external/YourVouchers.vue'
 import PurchaseVoucher from '@/components/external/PurchaseVoucher.vue'
 import PurchasePass from '@/components/external/PurchasePass.vue'
 import Cart from '@/components/external/Cart.vue'
@@ -17,29 +22,54 @@ export default {
             name: 'external'
         },
         {
-            path: '/faq',
-            component: FAQs,
-            name: 'faqs'
-        },
-        {
-            path: '/purchase-voucher',
-            component: PurchaseVoucher,
+            path: '/purchase-voucher/',
+            component: ShopHome,
             name: 'purchase-voucher'
         },
         {
-            path: '/purchase-pass/:passTypeId',
-            component: PurchasePass,
+            path: '/purchase-pass/:passTypeId/',
+            component: ShopHome,
             name: 'purchase-pass'
         },
         {
-            path: '/cart',
+            path: '/cart/',
             component: Cart,
             name: 'cart'
         },
         {
-            path: '/checkout-success/:uuid',
+            path: '/checkout-success/:uuid/',
             component: CheckoutSuccess,
             name: 'checkout-success'
+        },
+        {
+            path: '/your-park-passes/',
+            component: YourParkPasses,
+            name: 'your-park-passes'
+        },
+        {
+            path: '/your-vouchers/',
+            component: YourVouchers,
+            name: 'your-vouchers'
+        },
+        {
+            path: '/your-orders/',
+            component: YourOrders,
+            name: 'your-orders'
+        },
+        {
+            path: '/faq/',
+            component: FAQs,
+            name: 'faqs'
+        },
+        {
+            path: '/help/',
+            component: Help,
+            name: 'help'
+        },
+        {
+            path: '/contact/',
+            component: Contact,
+            name: 'contact'
         },
     ]
 }
