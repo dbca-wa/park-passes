7f08264b935742aff8ad36bcd687c787d5af1649
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-08-23 15:39:08 +0800

    Added ExternalVouchersView.

--- parkpasses/components/vouchers/views.py
+++ parkpasses/components/vouchers/views.py
@@ -1,5 +1,9 @@
 from django.views.generic.base import TemplateView
 
 
+class ExternalVouchersView(TemplateView):
+    template_name = "parkpasses/your-vouchers.html"
+
+
 class InternalVouchersView(TemplateView):
     template_name = "parkpasses/index.html"
