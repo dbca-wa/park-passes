796808e5f8909ff18ea88554b7e7f2f0d1dfe1cf
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-08-24 16:15:43 +0800

    Substantial work adding functionality to the checkout success page and tweaking visuals.

--- parkpasses/frontend/parkpasses/src/components/external/CheckoutSuccess.vue
+++ parkpasses/frontend/parkpasses/src/components/external/CheckoutSuccess.vue
@@ -1,34 +1,48 @@
 <template>
     <div class="container" id="shopHome">
-        <div class="row">
+        <div class="row ms-4 mb-3">
             <div class="col">
                 <h1>Checkout Success</h1>
             </div>
         </div>
-        <div class="row gx-4">
-            <div class="col-md-3 p-3">
-                <p>Maybe add some reassuring text here.</p>
-
-                <p>Mauris a purus lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-                Nam sollicitudin lectus lacus. Morbi id nibh in mauris pharetra porta. Aliquam at lectus massa.
-                Nulla elit dolor, fermentum quis dignissim id, finibus quis purus. Donec sed efficitur metus, nec sagittis neque.
-                Sed at aliquam turpis. Cras mauris felis, rhoncus nec turpis ac, eleifend imperdiet dui.</p>
-
-                <p>Proin aliquam pulvinar neque, pharetra cursus libero auctor eu. Ut hendrerit eros at nisl efficitur blandit.
-                Proin luctus gravida tortor. Fusce ut diam eu augue sollicitudin fringilla. Praesent accumsan ultricies porttitor.
-                Nam sollicitudin condimentum nisi, eget vulputate magna varius sollicitudin.</p>
-
-                <p>Integer vel erat aliquam, viverra dolor eu, interdum augue. Sed magna purus, accumsan id suscipit at, gravida ac nisl.</p>
-            </div>
-
-            <div class="col-md-8">
-
+        <div class="row ms-4">
+            <div class="col-md-3">
                 <div class="card">
-                    <h5 class="card-header">Your Order has been processed successfully</h5>
+                    <h5 class="card-header">Side menu</h5>
                     <div class="card-body">
                         <h5 class="card-title">Special title treatment</h5>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+
+                <div v-if="order" class="card bg-white">
+                    <div class="card-header checkout-success-item-header">
+                        <span>Order Placed</span>
+                        <span>{{ formattedOrderDate }}</span>
+
+                        <span>Items</span>
+                        <span>{{ order.items.length }}</span>
+
+                        <span>Total</span>
+                        <span>${{ order.total.toFixed(2) }}</span>
+
+                        <span>ORDER # {{ order.order_number }}</span>
+                        <span><a href="#">View Invoice</a></span>
+
+                    </div>
+                    <div class="card-body">
+
+                        <div v-for="orderItem in order.items" class="card bg-white mb-3">
+                            <div class="card-header order-item">
+                                <span>{{ orderItem.description }}</span>
+                                <span>${{ orderItem.amount }}</span>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
@@ -40,28 +54,72 @@
 </template>
 
 <script>
-import { apiEndpoints } from '@/utils/hooks'
+import { apiEndpoints, helpers } from '@/utils/hooks'
 import Loader from '@/utils/vue/Loader.vue'
 
 export default {
     name: "CheckoutSuccess",
     data: function () {
         return {
-            faqs: null
+            orderUUID: null,
+            order: null,
+            formattedOrderDate: null,
         };
     },
     components: {
         Loader
     },
     methods: {
-
+        fetchOrder: function (uuid) {
+            let vm = this;
+            vm.loading = true;
+            fetch(apiEndpoints.orderRetrieveExternal(uuid))
+                .then(async response => {
+                    const data = await response.json();
+                    if (!response.ok) {
+                        const error = (data && data.message) || response.statusText;
+                        console.log(error);
+                        return Promise.reject(error);
+                    }
+                    // Do something after adding the voucher to the database and the users cart
+                    vm.formattedOrderDate = helpers.getPrettyDateFromDatetime(data.datetime_created)
+                    vm.order = Object.assign({}, data);
+                    console.log(vm.order)
+                })
+                .catch(error => {
+                    vm.systemErrorMessage = "ERROR: Please try again in an hour.";
+                    console.error("There was an error!", error);
+                }).finally(() => {
+                    vm.loading = false;
+                });
+        },
     },
     created: function() {
-
+        this.orderUUID = this.$route.params.uuid;
+        this.fetchOrder(this.orderUUID);
     }
 };
 </script>
 
 <style scoped>
+    .checkout-success-item-header {
+        /* create a grid */
+        display: grid;
+
+        /* create colums. 1fr means use available space */
+        grid-template-rows: 1fr 1fr;
+        grid-template-columns: max-content max-content 1fr max-content;
+         grid-auto-flow: column;
+        align-items: center;
+        justify-content: center;
+        grid-gap: 2px 30px;
+        color: #565959;
+        font-size:12px;
+    }
+
+    .order-item {
+        display: grid;
+        grid-template-columns: 21fr max-content;
+    }
 
 </style>
