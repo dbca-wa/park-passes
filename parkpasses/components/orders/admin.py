cdcb09487052f7567cbb7b2e5a9ff5cb823971c3
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-08-24 16:06:56 +0800

    Added an OrderItem admin for debugging purposes. Added new fields to OrderAdmin.

--- parkpasses/components/orders/admin.py
+++ parkpasses/components/orders/admin.py
@@ -3,16 +3,30 @@ from django.contrib import admin
 from parkpasses.components.orders.models import Order, OrderItem
 
 
+class OrderItemAdmin(admin.ModelAdmin):
+    model = OrderItem
+
+
+admin.site.register(OrderItem, OrderItemAdmin)
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
 
 
 class OrderAdmin(admin.ModelAdmin):
     model = Order
+    fields = [
+        "order_number",
+        "user",
+        "uuid",
+        "invoice_reference",
+        "datetime_created",
+    ]
     list_display = (
         "order_number",
         "user",
-        "datetime_created",
+        "uuid",
     )
     readonly_fields = [
         "order_number",
