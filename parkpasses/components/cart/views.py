ea5732f1f78ce8302396020666fcba7fa5d20ca9
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-08-25 16:40:43 +0800

    Removed redundant code from CartView and moved the get_or_create_cart and set_user_for_cart_and_items calls to the get method instead.

--- parkpasses/components/cart/views.py
+++ parkpasses/components/cart/views.py
@@ -1,30 +1,23 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
-from django.views.generic import ListView
 from django.views.generic.base import TemplateView
 
-from parkpasses.components.cart.models import Cart, CartItem
+from parkpasses.components.cart.models import Cart
 from parkpasses.forms import LoginForm
-from parkpasses.helpers import is_internal
+from parkpasses.helpers import is_customer, is_internal
 
 
-class CartView(LoginRequiredMixin, ListView):
+class CartView(LoginRequiredMixin, TemplateView):
     template_name = "parkpasses/cart.html"
-    model = CartItem
-
-    def get_queryset(self):
-        cart_id = self.request.session.get("cart_id", None)
-        if cart_id and Cart.objects.filter(id=cart_id).exists():
-            cart = Cart.objects.get(id=cart_id)
-            cart.set_user_for_cart_and_items(self.request.user.id)
-            return CartItem.objects.filter(cart=cart)
-        else:
-            return CartItem.objects.none()
 
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
             if is_internal(self.request):
                 return redirect("internal")
+            if is_customer(self.request):
+                cart = Cart.get_or_create_cart(self.request)
+                if not cart.user:
+                    cart.set_user_for_cart_and_items(self.request.user.id)
         kwargs["form"] = LoginForm
         return super().get(*args, **kwargs)
 
