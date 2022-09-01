1d387df3f293f64353dd2bed2976a9186a6ddf2e
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-01 16:40:48 +0800

    Added some important constants.

--- parkpasses/frontend/parkpasses/src/utils/constants.js
+++ parkpasses/frontend/parkpasses/src/utils/constants.js
@@ -1,4 +1,8 @@
 module.exports = {
+    PARKPASSES_APP_LABEL:       'parkpasses',
+    PARKPASSES_MODELS_PASS:     'pass',
+
+    HOLIDAY_PASS_NAME: 'HOLIDAY_PASS',
     ERRORS: {
         // The critical error will be shown on the client side when a critical error
         // has occured on the backend and needs the attention of the system admins.
