55b39d9841dc5d0e28c57a5fca665588e6efa639
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-02 16:39:06 +0800

    New component to show the status in a side panel.

---
+++ parkpasses/frontend/parkpasses/src/components/common/StatusPanel.vue
@@ -0,0 +1,29 @@
+<template id="CommsLogs">
+    <div class="">
+        <div class="card card-default">
+            <div class="card-header">
+                Status
+            </div>
+            <div class="card-body card-collapse">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <strong>Status</strong><br/>
+                        {{ status }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PassForm",
+    props: {
+        status: {
+            required: true,
+            type: String,
+        }
+    },
+}
+</script>
