89d602de83171e12feb84f5f44e242ec068ee309
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-02 16:47:46 +0800

    Added a few important constants.

--- parkpasses/frontend/parkpasses/src/utils/constants.js
+++ parkpasses/frontend/parkpasses/src/utils/constants.js
@@ -1,6 +1,7 @@
 module.exports = {
-    PARKPASSES_APP_LABEL:       'parkpasses',
-    PARKPASSES_MODELS_PASS:     'pass',
+    PARKPASSES_APP_LABEL:               'parkpasses',
+    PARKPASSES_MODELS_USERACTION:       'useraction',
+    PARKPASSES_MODELS_PASS:             'pass',
 
     HOLIDAY_PASS_NAME: 'HOLIDAY_PASS',
     ERRORS: {
