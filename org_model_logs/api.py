cfe7ce0f0e208c8162e15d8aefd1e1f83925d6d4
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-02 16:46:40 +0800

    Added ordering and fleshed out the possible querysets that the UserActionList can return.

--- org_model_logs/api.py
+++ org_model_logs/api.py
@@ -2,7 +2,7 @@ import logging
 
 from django.contrib.contenttypes.models import ContentType
 from rest_framework import generics, viewsets
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework_datatables.filters import DatatablesFilterBackend
 from rest_framework_datatables.pagination import DatatablesPageNumberPagination
 
@@ -21,16 +21,34 @@ class UserActionList(generics.ListAPIView):
     pagination_class = DatatablesPageNumberPagination
     filter_backends = (
         SearchFilter,
+        OrderingFilter,
         DatatablesFilterBackend,
     )
+    ordering = ("-when",)
 
     def get_queryset(self):
         app_label = self.request.query_params.get("app_label", None)
         model = self.request.query_params.get("model", None)
-        if ContentType.objects.filter(app_label=app_label, model=model).exists():
-            content_type = ContentType.objects.get(app_label=app_label, model=model)
-            return UserAction.objects.filter(content_type=content_type)
-
+        object_id = self.request.query_params.get("object_id", None)
+        if app_label and model and object_id:
+            if ContentType.objects.filter(app_label=app_label, model=model).exists():
+                content_type = ContentType.objects.get(app_label=app_label, model=model)
+                logger.debug("content_type = " + str(content_type))
+                if UserAction.objects.filter(
+                    content_type=content_type, object_id=object_id
+                ).exists():
+                    return UserAction.objects.filter(
+                        content_type=content_type, object_id=object_id
+                    )
+                UserAction.objects.none()
+        if app_label and model:
+            if ContentType.objects.filter(app_label=app_label, model=model).exists():
+                content_type = ContentType.objects.get(app_label=app_label, model=model)
+                return UserAction.objects.filter(content_type=content_type)
+        if app_label:
+            if ContentType.objects.filter(app_label=app_label, model=model).exists():
+                content_types = ContentType.objects.filter(app_label=app_label)
+                return UserAction.objects.filter(content_type__in=content_types)
         return UserAction.objects.all()
 
     def list(self, request, *args, **kwargs):
