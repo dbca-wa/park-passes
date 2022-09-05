4e2f561585270e27402c48356c53ca91b9b5d7a8
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-05 10:44:01 +0800

    Added ability to show general help text on help page.

--- parkpasses/frontend/parkpasses/src/components/external/Help.vue
+++ parkpasses/frontend/parkpasses/src/components/external/Help.vue
@@ -7,8 +7,17 @@
         </div>
         <div class="row">
             <div class="col p-3">
-                <div id="help">
-                    <p>The help page will go here.</p>
+                <div v-if="generalHelp" v-html="generalHelp.content" id="generalHelp">
+                </div>
+
+                <div v-if="systemErrorMessage" class="alert alert-danger" role="alert">
+                    {{ systemErrorMessage }}
+                </div>
+
+                <div v-if="loading" class="d-flex justify-content-center mt-5">
+                    <div class="spinner-border text-primary" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -24,19 +33,40 @@ export default {
     name: "Help",
     data: function () {
         return {
-            faqs: null
+            generalHelp: null,
+            loading: false,
+            systemErrorMessage: null,
         };
     },
     components: {
         Loader
     },
     methods: {
-
+        fetchGeneralHelp: function () {
+            let vm = this;
+            vm.loading = true;
+            fetch(apiEndpoints.helpDetailByLabel('general'))
+            .then(async response => {
+                const data = await response.json();
+                if (!response.ok) {
+                    const error = (data && data.message) || response.statusText;
+                    console.log(error)
+                    return Promise.reject(error);
+                }
+                vm.generalHelp = data;
+                vm.loading = false;
+            })
+            .catch(error => {
+                this.systemErrorMessage = "ERROR: Please try again in an hour.";
+                console.error("There was an error!", error);
+            });
+        }
     },
     created: function() {
-
+        this.fetchGeneralHelp();
+    }
 }
-};
+
 </script>
 
 <style scoped>
