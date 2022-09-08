6ab3dd403579f27f21ca9bbba9d276290f1cabf6
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-08 16:51:42 +0800

    Made ExternalOrdersView require login.

--- parkpasses/components/orders/views.py
+++ parkpasses/components/orders/views.py
@@ -1,5 +1,15 @@
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 
 
-class ExternalOrdersView(TemplateView):
+class ExternalOrdersView(LoginRequiredMixin, TemplateView):
     template_name = "parkpasses/your-orders.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["dev"] = settings.DEV_STATIC
+        context["dev_url"] = settings.DEV_STATIC_URL
+        if hasattr(settings, "DEV_APP_BUILD_URL") and settings.DEV_APP_BUILD_URL:
+            context["app_build_url"] = settings.DEV_APP_BUILD_URL
+        return context
