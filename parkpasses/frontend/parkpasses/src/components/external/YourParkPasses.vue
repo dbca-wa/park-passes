e95b63db2bf82c4e2471f050fb403adabc85bb79
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-08 16:54:33 +0800

    A decent chunk of work configuring infinite scrolling of nice cards for your passes page.

--- parkpasses/frontend/parkpasses/src/components/external/YourParkPasses.vue
+++ parkpasses/frontend/parkpasses/src/components/external/YourParkPasses.vue
@@ -3,54 +3,60 @@
 
     <h2 class="px-4 pb-3">Your Park Passes</h2>
 
-    <div v-if="loading" class="d-flex justify-content-center mt-5">
-        <div class="spinner-border text-primary" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div>
-    </div>
-
-    <div v-else class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-1 row-cols-xl-2 row-cols-xxl-2 g-4 px-4">
+    <div v-if="passes" id="passes" class="passes row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-1 row-cols-xl-2 row-cols-xxl-2 g-4 px-4">
         <div v-for="pass in passes" class="col-xs">
           <div class="card border-bottom-0 rounded-0 rounded-top">
             <img :src="pass.pass_type_image" class="img-fluid float-left" width="300" />
             <div class="card-body">
               <h5 class="card-title">{{ pass.pass_type }}</h5>
               <p class="card-text mt-3 rounded-bottom-0">
-                <div class="border-bottom">Start Date:</div><div class="border-bottom ps-1">{{ pass.date_start }}</div>
-                <div class="border-bottom">Expiry Date:</div><div class="border-bottom ps-1">{{ pass.date_expiry }}</div>
-                <label class="orm-check-label mt-2">Renew Automatically</label>
-                <div class="form-check form-switch mt-2 mx-auto">
-                  <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" v-model="pass.renew_automatically">
+                <div class="border-bottom">{{  passCurrentOrFuture(pass) ? 'Start Date' : 'Started' }}:</div>
+                <div class="border-bottom ps-1">{{ pass.date_start }}</div>
+                <div class="border-bottom">{{  passCurrentOrFuture(pass) ? 'Expiry Date' : 'Expired' }}:</div>
+                <div class="border-bottom ps-1">{{ pass.date_expiry }}</div>
+                <label v-if="passCurrentOrFuture(pass)" class="orm-check-label mt-2">Renew Automatically</label>
+                <div v-if="passCurrentOrFuture(pass)" class="form-check form-switch mt-2 mx-auto">
+                  <input @change="updatePass(pass)" class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" v-model="pass.renew_automatically">
                 </div>
               </p>
             </div>
           </div>
           <div class="card-footer border-top-0 align-items-center">
             <div><span class="badge" :class="statusClass(pass)">{{ pass.processing_status_display_name }}</span></div>
-            <!--
-            <button type="button" class="btn licensing-btn-primary btn-sm"><i class="fa-solid fa-car fa-lg"></i> Update Vehicle Rego</button>
-            <button type="button" class="btn licensing-btn-primary btn-sm"><i class="fa-solid fa-file-invoice fa-lg"></i> Download Invoice</button>
-            <button type="button" class="btn licensing-btn-primary btn-sm"><i class="fa-solid fa-file-pdf fa-lg"></i> Download Pass</button>
-            -->
-            <div class="link"><a @click="" data-bs-toggle="collapse" :href="'#updateVehicleRego'+pass.id" role="button" aria-expanded="false" :aria-controls="'updateVehicleRego'+pass.id">Update Vehicle Rego</a> <i class="fa-solid fa-car fa-lg ps-1"></i></div>
-            <div class="link"><a href="">Download Invoice</a> <i class="fa-solid fa-file-invoice fa-lg ps-1"></i></div>
-            <div class="link"><a :href="passURL(pass.id)" target="blank">Download Pass</a> <i class="fa-solid fa-file-pdf fa-lg ps-1"></i></div>
+            <div v-if="canUpdateVehicleDetails(pass)" class="link"><a data-bs-toggle="collapse" :href="'#updateVehicleRego'+pass.id" role="button" aria-expanded="false" :aria-controls="'updateVehicleRego'+pass.id">Update Vehicle Rego</a> <i class="fa-solid fa-car fa-lg ps-1"></i></div>
+            <div v-else></div>
+            <div v-if="passCurrentOrFuture(pass)" class="link"><a :href="passURL(pass.id)" target="blank">Download Pass PDF</a> <i class="fa-solid fa-file-pdf fa-lg ps-1"></i></div>
+            <div v-else></div>
+            <div v-if="pass.invoice_link" class="link"><a target="blank" :href="pass.invoice_link">Download Invoice</a> <i class="fa-solid fa-file-invoice fa-lg ps-1"></i></div>
+            <div v-else></div>
+
           </div>
-          <div :id="'updateVehicleRego'+pass.id" class="showUpdateVehicleRego collapse">
+          <div v-if="canUpdateVehicleDetails(pass)" :id="'updateVehicleRego'+pass.id" class="showUpdateVehicleRego collapse">
 
             <div class="row">
               <div class="col">
-                <input type="text" class="form-control form-control-sm m-3" placeholder="Vehicle 1 Registration" aria-label="Vehicle Registration 1" v-model="pass.vehicle_registration_1">
+                <input type="text" class="form-control form-control-sm m-3" placeholder="Vehicle 1 Registration" aria-label="Vehicle Registration 1" v-model="pass.vehicle_registration_1" maxlength="10">
               </div>
               <div class="col">
-                <input type="text" class="form-control form-control-sm m-3" placeholder="Vehicle 2 Registration" aria-label="Vehicle Registration 2" v-model="pass.vehicle_registration_2">
+                <input type="text" class="form-control form-control-sm m-3" placeholder="Vehicle 2 Registration" aria-label="Vehicle Registration 2" v-model="pass.vehicle_registration_2" maxlength="10">
               </div>
               <div class="col">
-                <button class="btn btn-sm licensing-btn-primary float-end m-3">Update</button>
+                <button v-if="!loadingUpdatePass" @click="updatePass(pass)" class="btn btn-sm licensing-btn-primary float-end m-3 px-5">Update</button>
+                <button v-else class="btn btn-sm licensing-btn-primary float-end m-3 px-5">
+                    <div class="spinner-border text-light" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </button>
               </div>
             </div>
 
           </div>
+        </div>
+    </div>
+
+    <div v-if="loading" class="d-flex justify-content-center mt-5">
+        <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
         </div>
     </div>
 
@@ -58,21 +64,28 @@
 </template>
 
 <script>
-import { apiEndpoints } from '@/utils/hooks'
+import { apiEndpoints, helpers } from '@/utils/hooks'
+import Swal from 'sweetalert2'
 
 export default {
   name: "externalDashboard",
   data: function () {
     return {
-      loading: false,
-      passes: null,
+        loading: false,
+        loadingMore: false,
+        loadingUpdatePass: false,
+        passes: null,
+        pageIndex: 0,
+        count: null,
     }
   },
   components: {
 
   },
   computed: {
-
+      allResultsLoaded() {
+          return this.pageIndex * 10 >= this.count;
+      }
   },
   methods: {
     statusClass: function (pass) {
@@ -92,10 +105,26 @@ export default {
       console.log('showUpdateVehicleRego');
       $(`#updateVehicleRego${passId}`).slideDown();
     },
+    passCurrentOrFuture: function (pass) {
+      switch (pass.processing_status) {
+        case 'CU':
+          return true;
+        case 'FU':
+          return true;
+        default:
+          return false;
+      }
+    },
+    canUpdateVehicleDetails: function (pass) {
+      return this.passCurrentOrFuture(pass) && !pass.prevent_further_vehicle_updates;
+    },
     fetchPasses: function () {
       let vm = this;
-      vm.loading = true;
-      fetch(apiEndpoints.passesListExternal)
+      vm.pageIndex++;
+      if(!vm.passes){
+          vm.loading = true;
+      }
+      fetch(apiEndpoints.passesListExternal + '?page=' + vm.pageIndex)
         .then(async response => {
           const data = await response.json();
           if (!response.ok) {
@@ -103,21 +132,73 @@ export default {
             console.log(error)
             return Promise.reject(error);
           }
-          vm.passes = data.results
+          if(vm.passes){
+              vm.passes.push(...data.results)
+          } else {
+              vm.passes = data.results
+          }
+          vm.count = data.count
           vm.loading = false;
+          vm.loadingMore = false;
         })
         .catch(error => {
           this.systemErrorMessage = "ERROR: Please try again in an hour.";
           console.error("There was an error!", error);
         });
     },
+    handleScroll: function(e) {
+        let vm = this;
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            console.log('At bottom of page');
+        }
+    },
+    updatePass: function (pass) {
+        let vm = this;
+        vm.loadingUpdatePass = true;
+        pass.csrfmiddlewaretoken = helpers.getCookie('csrftoken');
+        const requestOptions = {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(pass)
+        };
+        fetch(apiEndpoints.updatePassExternal(pass.id), requestOptions)
+        .then(async response => {
+            const data = await response.json();
+            if (!response.ok) {
+                const error = (data && data.message) || response.statusText;
+                console.log(error);
+                return Promise.reject(error);
+            }
+            Swal.fire({
+                title: 'Success',
+                text: 'Park Pass updated successfully.',
+                icon: 'success',
+                confirmButtonText: 'OK'
+            })
+            vm.loadingUpdatePass = false;
+        })
+        .catch(error => {
+            this.systemErrorMessage = "ERROR: Please try again in an hour.";
+            console.error("There was an error!", error);
+        });
+    },
   },
   created: function () {
-    this.fetchPasses();
+      this.fetchPasses();
   },
   mounted: function () {
-
+    let vm = this;
+    window.onscroll = async function(ev) {
+        if(!vm.loadingMore && !vm.allResultsLoaded) {
+            let element = document.getElementById('passes');
+            if (element.getBoundingClientRect().bottom < window.innerHeight) {
+                vm.loadingMore = true;
+                await vm.fetchPasses();
+            }
+        }
+    };
   },
+
 };
 </script>
 
