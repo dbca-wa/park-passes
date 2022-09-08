502fb35b1ff8c19f9402032a303fb32b5af719b8
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-08 16:53:12 +0800

    Added LoginRequiredMixin to ExternalPassView.

--- parkpasses/components/passes/views.py
+++ parkpasses/components/passes/views.py
@@ -1,8 +1,18 @@
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 
 
-class ExternalPassView(TemplateView):
+class ExternalPassView(LoginRequiredMixin, TemplateView):
     template_name = "parkpasses/your-park-passes.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["dev"] = settings.DEV_STATIC
+        context["dev_url"] = settings.DEV_STATIC_URL
+        if hasattr(settings, "DEV_APP_BUILD_URL") and settings.DEV_APP_BUILD_URL:
+            context["app_build_url"] = settings.DEV_APP_BUILD_URL
+        return context
 
 
 class InternalPricingWindowsView(TemplateView):
