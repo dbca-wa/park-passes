1b6ef4f9f5b86a98e15a25320e2ac85342808787
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-09 10:16:34 +0800

    Added DATATABLE_PROCESSING_HTML constant so it doesn't have to be changed in multiple places.

--- parkpasses/frontend/parkpasses/src/utils/constants.js
+++ parkpasses/frontend/parkpasses/src/utils/constants.js
@@ -4,6 +4,9 @@ module.exports = {
     PARKPASSES_MODELS_PASS:             'pass',
 
     HOLIDAY_PASS_NAME: 'HOLIDAY_PASS',
+
+    DATATABLE_PROCESSING_HTML: '<div class="spinner-border org-primary align-items-center" style="width: 3rem; height: 3rem; margin-top:45px;" role="status"><span class="visually-hidden">Loading...</span></div>',
+
     ERRORS: {
         // The critical error will be shown on the client side when a critical error
         // has occured on the backend and needs the attention of the system admins.
