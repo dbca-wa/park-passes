0df3c7f9c82dc8065ec4df5587b8331773e53af2
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-14 16:36:57 +0800

    Added park passes specific EntryTypeList and CreateCommunicationsLogEntry.

--- parkpasses/components/main/api.py
+++ parkpasses/components/main/api.py
@@ -1,33 +1,53 @@
 import logging
 
 from org_model_documents.api import DocumentCreateView, DocumentViewSet
-from org_model_logs.api import UserActionList, UserActionViewSet
-from parkpasses.components.main.serializers import UserActionSerializer
+from org_model_logs.api import (
+    CreateCommunicationsLogEntry,
+    EntryTypeList,
+    UserActionList,
+    UserActionViewSet,
+)
+from org_model_logs.serializers import EntryTypeSerializer
+from parkpasses.components.main.serializers import (
+    CommunicationsLogEntrySerializer,
+    UserActionSerializer,
+)
 from parkpasses.permissions import IsInternal
 
 logger = logging.getLogger(__name__)
 
 
-class DocumentCreateView(DocumentCreateView):
-    """The permission classes belong to park passes so can't be included in the org_model_documents app"""
+""" The following classes are overrideen here because the permission classes belong to park passes app
+so can't be included in the org_model_documents or org_model_logs apps as it would break their independence"""
 
+
+class DocumentCreateView(DocumentCreateView):
     permission_classes = [IsInternal]
 
 
 class DocumentViewSet(DocumentViewSet):
-    """The permission classes belong to park passes so can't be included in the org_model_documents app"""
+    permission_classes = [IsInternal]
 
+
+class EntryTypeList(EntryTypeList):
+    serializer_class = EntryTypeSerializer
     permission_classes = [IsInternal]
 
 
 class UserActionList(UserActionList):
-    """The permission classes belong to park passes so can't be included in the org_model_logs app"""
-
     serializer_class = UserActionSerializer
     permission_classes = [IsInternal]
 
 
 class UserActionViewSet(UserActionViewSet):
-    """The permission classes belong to park passes so can't be included in the org_model_logs app"""
-
     permission_classes = [IsInternal]
+
+
+class CreateCommunicationsLogEntry(CreateCommunicationsLogEntry):
+    permission_classes = [IsInternal]
+
+    def get_serializer_class(self):
+        logger.debug("self.request.method = " + str(self.request.method))
+        if "GET" == self.request.method:
+            return CommunicationsLogEntrySerializer
+        return super().get_serializer_class()
