350a75816bb68d109da0d3ccabab69e07d992bbf
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-14 16:35:27 +0800

    Added park passes specific customisations of CommunicationsLogEntrySerializer.

--- parkpasses/components/main/serializers.py
+++ parkpasses/components/main/serializers.py
@@ -3,7 +3,10 @@ from rest_framework import serializers
 
 from org_model_documents.models import Document
 from org_model_documents.serializers import DocumentSerializer
-from org_model_logs.serializers import UserActionSerializer
+from org_model_logs.serializers import (
+    CommunicationsLogEntrySerializer,
+    UserActionSerializer,
+)
 from parkpasses.ledger_api_utils import retrieve_email_user
 
 
@@ -13,6 +16,22 @@ class UserActionSerializer(UserActionSerializer):
 
     def get_who(self, obj):
         return retrieve_email_user(obj.who).get_full_name()
+
+    def get_documents(self, obj):
+        documents = []
+        content_type = ContentType.objects.get_for_model(obj)
+        if Document.objects.filter(
+            content_type=content_type, object_id=obj.id
+        ).exists():
+            documents = Document.objects.filter(
+                content_type=content_type, object_id=obj.id
+            )
+            documents = DocumentSerializer(documents, many=True).data
+        return documents
+
+
+class CommunicationsLogEntrySerializer(CommunicationsLogEntrySerializer):
+    documents = serializers.SerializerMethodField()
 
     def get_documents(self, obj):
         documents = []
