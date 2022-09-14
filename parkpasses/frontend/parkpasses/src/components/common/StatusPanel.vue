03f99417a4bc92d355da55a0afd6a45219b004c2
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-14 16:33:06 +0800

    Added code to show a badge with the appropriate colour depending on the status.

--- parkpasses/frontend/parkpasses/src/components/common/StatusPanel.vue
+++ parkpasses/frontend/parkpasses/src/components/common/StatusPanel.vue
@@ -8,7 +8,12 @@
                 <div class="row">
                     <div class="col-sm-12">
                         <strong>Status</strong><br/>
-                        {{ status }}
+                        <template v-if="badge">
+                            <span :class="'badge ' + helpers.getBadgeClass(status)">{{ status }}</span>
+                        </template>
+                        <template v-else>
+                            {{ status }}
+                        </template>
                     </div>
                 </div>
             </div>
@@ -17,13 +22,41 @@
 </template>
 
 <script>
+import { helpers } from '@/utils/hooks'
+
 export default {
+
     name: "PassForm",
     props: {
         status: {
             required: true,
             type: String,
+        },
+        badge: {
+            required: false,
+            type: Boolean,
+            default: false,
         }
     },
+    methods: {
+        getBadgeClass: function () {
+            switch(this.status) {
+                case 'Current':
+                    return 'bg-success'
+                    break;
+                case 'Future':
+                    return 'bg-info';
+                    break;
+                case 'Expired':
+                    return 'bg-danger';
+                    break;
+                case 'Cancelled':
+                    return 'bg-danger';
+                    break;
+                default:
+                    return 'org-badge-primary';
+            }
+        }
+    }
 }
 </script>
