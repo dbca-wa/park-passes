7039ed65eb3480953993db0f915bcd1885bbbd0c
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-19 16:52:43 +0800

    Added new retailer routes.

--- parkpasses/frontend/parkpasses/src/components/retailer/routes/index.js
+++ parkpasses/frontend/parkpasses/src/components/retailer/routes/index.js
@@ -1,6 +1,7 @@
 import { RouterView } from 'vue-router'
 
 import RetailerDashboard from '@/components/retailer/Dashboard.vue'
+import SellPass from '@/components/retailer/SellPass.vue'
 
 export default {
     path: '/retailer',
@@ -11,6 +12,26 @@ export default {
             path: '/retailer',
             component: RetailerDashboard,
             name: 'retailer'
+        },
+        {
+            path: '/retailer/parkpasses',
+            component: RetailerDashboard,
+            name: 'retailer-park-passes'
+        },
+        {
+            path: '/retailer/reports',
+            component: RetailerDashboard,
+            name: 'retailer-park-passes'
+        },
+        {
+            path: '/retailer/sell-a-pass',
+            component: SellPass,
+            name: 'sell-a-pass'
+        },
+        {
+            path: '/retailer/sell-a-pass/:passTypeId/',
+            component: SellPass,
+            name: 'sell-a-pass-by-id'
         },
     ]
 }
