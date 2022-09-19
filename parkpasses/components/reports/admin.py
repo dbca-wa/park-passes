bb18fdcfccd36602c4720ee33adb54d04c37d0eb
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-19 16:58:30 +0800

    Added basic config of reports admin.

--- parkpasses/components/reports/admin.py
+++ parkpasses/components/reports/admin.py
@@ -1,3 +1,12 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from parkpasses.components.reports.models import Report
+
+
+class ReportAdmin(admin.ModelAdmin):
+    model = Report
+    list_display = [field.name for field in Report._meta.get_fields()]
+    ordering = ["-datetime_created"]
+
+
+admin.site.register(Report, ReportAdmin)
