d2e46364b01c54f0881eaf9a52418008da72cfff
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-28 09:15:57 +0800

    Moved org_model_documents to it's own repo / published to pypi.

--- org_model_documents/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrgModelDocumentsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "org_model_documents"
-    verbose_name = "Park Pass Model Documents"
