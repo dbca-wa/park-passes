a96f8497fdce91af921c32ec661f07e887ea19ce
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-29 16:01:52 +0800

    Added assign_orphan_passes method that assigns any passes with the users email address that aren't yet assigned to a user. This covers the situation where a retailer sells a pass to a user that doesn't yet have a ledger account. Then if they need to log in to update their vehicle details for example they can do so.

--- parkpasses/components/users/signals.py
+++ parkpasses/components/users/signals.py
@@ -1,13 +1,26 @@
 from django.contrib.auth.signals import user_logged_in
 
 from parkpasses.components.cart.models import Cart
+from parkpasses.components.passes.models import Pass
 
 
 def init_cart(sender, user, request, **kwargs):
+    """Since items can be added to a cart anonymously
+    we need to attach the correct user to those items once they log in"""
     cart = Cart.get_or_create_cart(request)
-    # Since items can be added to a cart anonymously
-    # we need to attach the correct user to those items once they log in
     cart.set_user_for_cart_and_items(request.user.id)
 
 
+def assign_orphan_passes(sender, user, request, **kwargs):
+    """When passes are sold via retailers, the user doesn't necessarily have an account in ledger
+    so we need a way to attach the user to those passes when they log in"""
+    email_user = request.user
+    queryset = Pass.objects.filter(user__isnull=True, email=email_user.email)
+    if queryset.exists():
+        for park_pass in queryset:
+            park_pass.user = email_user.id
+            park_pass.save()
+
+
 user_logged_in.connect(init_cart)
+user_logged_in.connect(assign_orphan_passes)
