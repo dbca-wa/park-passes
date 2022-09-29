d9c775a33534eeb3fe7122d9356149c74bb66553
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-09-29 16:08:00 +0800

    Further config of report serializers.

--- parkpasses/components/reports/serializers.py
+++ parkpasses/components/reports/serializers.py
@@ -1,10 +1,13 @@
+import os
+
 from rest_framework import serializers
 
 from parkpasses.components.reports.models import Report
 
 
 class RetailerReportSerializer(serializers.ModelSerializer):
-    processing_status = serializers.SerializerMethodField()
+    invoice_filename = serializers.SerializerMethodField()
+    processing_status_display = serializers.SerializerMethodField()
     datetime_created = serializers.SerializerMethodField()
     retailer_group = serializers.CharField(source="retailer_group.name")
 
@@ -12,14 +15,31 @@ class RetailerReportSerializer(serializers.ModelSerializer):
         model = Report
         fields = "__all__"
 
-    def get_processing_status(self, obj):
+    def get_processing_status_display(self, obj):
         return obj.get_processing_status_display()
+
+    def get_invoice_filename(self, obj):
+        return os.path.basename(obj.invoice.name)
 
     def get_datetime_created(self, obj):
         return obj.datetime_created.strftime("%d/%m/%Y")
 
 
 class InternalReportSerializer(serializers.ModelSerializer):
+    invoice_filename = serializers.SerializerMethodField()
+    processing_status_display = serializers.SerializerMethodField()
+    datetime_created = serializers.SerializerMethodField()
+    retailer_group = serializers.CharField(source="retailer_group.name")
+
     class Meta:
         model = Report
         fields = "__all__"
+
+    def get_processing_status_display(self, obj):
+        return obj.get_processing_status_display()
+
+    def get_invoice_filename(self, obj):
+        return os.path.basename(obj.invoice.name)
+
+    def get_datetime_created(self, obj):
+        return obj.datetime_created.strftime("%d/%m/%Y")
