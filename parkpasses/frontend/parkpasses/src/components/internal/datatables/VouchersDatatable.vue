7539a107df2694456d7abf32356b03c46c45aa9b
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-04 16:32:20 +0800

    Added coloured badges to processing_status field.

--- parkpasses/frontend/parkpasses/src/components/internal/datatables/VouchersDatatable.vue
+++ parkpasses/frontend/parkpasses/src/components/internal/datatables/VouchersDatatable.vue
@@ -240,7 +240,18 @@ export default {
             return {
                 data: "processing_status",
                 visible: true,
-                name: 'processing_status'
+                name: 'processing_status',
+                'render': function(row, type, full){
+                    let html = '';
+                    if('Delivered'==full.processing_status){
+                        html = `<span class="badge bg-success">${full.processing_status}</span>`;
+                    } else  if('New'==full.processing_status) {
+                        html = `<span class="badge org-badge-primary">${full.processing_status}</span>`;
+                    } else {
+                        html = `<span class="badge bg-danger">${full.processing_status}</span>`;
+                    }
+                    return html;
+                }
             }
         },
         columnInvoice: function(){
