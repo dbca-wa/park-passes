64cfa1c5f940767be6b00dbbcaddee81df61cb7d
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-04 16:09:09 +0800

    Added routes for retailer group users and retailer group invites.

--- parkpasses/components/retailers/urls.py
+++ parkpasses/components/retailers/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import url
 from rest_framework import routers
 
 from parkpasses.components.retailers.api import (
+    InternalRetailerGroupInviteViewSet,
+    InternalRetailerGroupUserViewSet,
     InternalRetailerGroupViewSet,
     RetailerGroupsForUser,
+    RetailerRetailerGroupInviteViewSet,
 )
 
 router = routers.SimpleRouter()
@@ -12,9 +15,26 @@ router.register(
     InternalRetailerGroupViewSet,
     basename="retailer-groups-internal",
 )
+router.register(
+    r"internal/retailer-group-users",
+    InternalRetailerGroupUserViewSet,
+    basename="retailer-group-users-internal",
+)
+router.register(
+    r"internal/retailer-group-invites",
+    InternalRetailerGroupInviteViewSet,
+    basename="retailer-group-invites-internal",
+)
+router.register(
+    r"retailer/retailer-group-invites",
+    RetailerRetailerGroupInviteViewSet,
+    basename="retailer-group-invites-retailer",
+)
+
 
 urlpatterns = [
     url(r"retailer-groups-for-user", RetailerGroupsForUser.as_view()),
+    url(r"retailer-invite-reponse", RetailerGroupsForUser.as_view()),
 ]
 
 urlpatterns += router.urls
