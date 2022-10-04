b0d78ebfcfdf946def988b5cc8428c7313aa0da6
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-04 16:13:50 +0800

    Added api request to return report pdf.

--- parkpasses/components/reports/api.py
+++ parkpasses/components/reports/api.py
@@ -82,6 +82,20 @@ class RetailerReportViewSet(viewsets.ModelViewSet):
                     return FileResponse(report.invoice)
         raise Http404
 
+    @action(methods=["GET"], detail=True, url_path="retrieve-report-pdf")
+    def retrieve_report_pdf(self, request, *args, **kwargs):
+        if RetailerGroupUser.objects.filter(
+            emailuser__id=self.request.user.id
+        ).exists():
+            retailer_groups = RetailerGroupUser.objects.filter(
+                emailuser__id=self.request.user.id
+            ).values_list("retailer_group__id", flat=True)
+            report = self.get_object()
+            if report.retailer_group.id in list(retailer_groups):
+                if report.report:
+                    return FileResponse(report.report)
+        raise Http404
+
 
 class ReportFilterBackend(DatatablesFilterBackend):
     """
@@ -137,4 +151,11 @@ class InternalReportViewSet(viewsets.ModelViewSet):
         report = self.get_object()
         if report.invoice:
             return FileResponse(report.invoice)
+        raise Http404
+
+    @action(methods=["GET"], detail=True, url_path="retrieve-report-pdf")
+    def retrieve_report_pdf(self, request, *args, **kwargs):
+        report = self.get_object()
+        if report.report:
+            return FileResponse(report.report)
         raise Http404
