58f1b93d2e1441419262ac56743a822b2e290c86
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-04 16:02:35 +0800

    Added a view to restrict access to the respond to retailer invite to those that have the right user email and access to the url with the right uuid for the invite.

--- parkpasses/components/retailers/views.py
+++ parkpasses/components/retailers/views.py
@@ -1,3 +1,27 @@
-# from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from parkpasses.components.retailers.models import RetailerGroupInvite
+
+
+class RespondToRetailUserInviteView(
+    LoginRequiredMixin, UserPassesTestMixin, TemplateView
+):
+    template_name = "parkpasses/repond-to-retailer-invite.html"
+
+    def test_func(self):
+        if self.request.user.is_authenticated:
+            uuid = self.kwargs["uuid"]
+            email = self.request.user.email
+            if RetailerGroupInvite.objects.filter(uuid=uuid, email=email).exists():
+                return True
+        return False
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["dev"] = settings.DEV_STATIC
+        context["dev_url"] = settings.DEV_STATIC_URL
+        if hasattr(settings, "DEV_APP_BUILD_URL") and settings.DEV_APP_BUILD_URL:
+            context["app_build_url"] = settings.DEV_APP_BUILD_URL
+        return context
