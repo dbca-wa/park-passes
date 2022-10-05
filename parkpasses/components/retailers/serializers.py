95ac07701a96271e8ac342a836f86db4c966ab7e
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-05 16:51:11 +0800

    More configuration of serializers.

--- parkpasses/components/retailers/serializers.py
+++ parkpasses/components/retailers/serializers.py
@@ -55,19 +55,44 @@ class RetailerGroupInviteSerializer(serializers.ModelSerializer):
         source="retailer_group.name", read_only=True
     )
     status_display = serializers.SerializerMethodField()
+    datetime_created = serializers.SerializerMethodField(read_only=True)
+    datetime_updated = serializers.SerializerMethodField(read_only=True)
+    is_admin = serializers.CharField(write_only=True, required=False, allow_blank=True)
 
     class Meta:
         model = RetailerGroupInvite
         fields = [
             "id",
+            "uuid",
             "user",
             "email",
             "retailer_group",
             "retailer_group_name",
             "status",
             "status_display",
+            "is_admin",
             "datetime_created",
+            "datetime_updated",
         ]
+        datatables_always_serialize = "status"
+
+    def create(self, validated_data):
+        validated_data.pop("is_admin", None)
+        return super().create(validated_data)
+
+    def update(self, validated_data):
+        validated_data.pop("is_admin", None)
+        return super().update(validated_data)
 
     def get_status_display(self, obj):
         return obj.get_status_display()
+
+    def get_datetime_created(self, obj):
+        return date_format(
+            obj.datetime_created, format="SHORT_DATETIME_FORMAT", use_l10n=True
+        )
+
+    def get_datetime_updated(self, obj):
+        return date_format(
+            obj.datetime_updated, format="SHORT_DATETIME_FORMAT", use_l10n=True
+        )
