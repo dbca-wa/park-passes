3c2f6d52b235b271f1d28a02a99cc97c8c54dfc9
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-06 15:56:08 +0800

    Modified email template classes to assign instance variables.

--- parkpasses/components/vouchers/emails.py
+++ parkpasses/components/vouchers/emails.py
@@ -11,27 +11,28 @@ logger = logging.getLogger(__name__)
 
 
 class VoucherSentPurchaserNotificationEmail(TemplateEmailBase):
-    subject = "Park Pass Voucher Sent"
-    html_template = "parkpasses/emails/voucher_purchaser_notification.html"
-    txt_template = "parkpasses/emails/voucher_purchaser_notification.txt"
+    def __init__(self, recipient_name):
+        super().__init__()
+        self.subject = f"Your Park Pass Voucher Sent to {recipient_name}"
+        self.html_template = "parkpasses/emails/voucher_purchaser_notification.html"
+        self.txt_template = "parkpasses/emails/voucher_purchaser_notification.txt"
 
 
 class VoucherRecipientNotificationEmail(TemplateEmailBase):
     def __init__(self, purchaser_name):
+        super().__init__()
         self.subject = "You have received a park pass voucher from {}".format(
             purchaser_name
         )
-        super().__init__()
-
-    html_template = "parkpasses/emails/voucher_recipient_notification.html"
-    txt_template = "parkpasses/emails/voucher_recipient_notification.txt"
+        self.html_template = "parkpasses/emails/voucher_recipient_notification.html"
+        self.txt_template = "parkpasses/emails/voucher_recipient_notification.txt"
 
 
 class VoucherEmails:
     @classmethod
     def send_voucher_purchase_notification_email(self, voucher):
         purchaser = voucher.get_purchaser
-        email = VoucherSentPurchaserNotificationEmail()
+        email = VoucherSentPurchaserNotificationEmail(voucher.recipient_name)
         context = {
             "voucher": voucher,
             "purchaser": purchaser,
