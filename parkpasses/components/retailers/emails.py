515504bd323226f4012eee1ba2c85e70bd2449ac
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-07 10:53:11 +0800

    Further config of emails for retailers.

--- parkpasses/components/retailers/emails.py
+++ parkpasses/components/retailers/emails.py
@@ -11,15 +11,49 @@ logger = logging.getLogger(__name__)
 
 
 class RetailerGroupUserInviteNotificationEmail(TemplateEmailBase):
-    def __init__(self, retailer_group_name):
+    def __init__(self, SYSTEM_NAME, retailer_group_name):
         super().__init__()
-        self.subject = (
-            f"You have been invited to join the {retailer_group_name} retailer group"
-        )
+        self.subject = f"You have been invited to the { SYSTEM_NAME } Retailer Group {retailer_group_name}"
         self.html_template = (
             "parkpasses/emails/retailer/retailer_group_user_invite.html"
         )
         self.txt_template = "parkpasses/emails/retailer/retailer_group_user_invite.txt"
+
+
+class RetailerGroupUserAcceptedNotificationEmail(TemplateEmailBase):
+    def __init__(self, email, retailer_group_name):
+        super().__init__()
+        self.subject = f"Retailer Group User {email} has accepted invite to {retailer_group_name} Retailer Group"
+        self.html_template = (
+            "parkpasses/emails/internal/retailer_group_user_invite_accepted.html"
+        )
+        self.txt_template = (
+            "parkpasses/emails/internal/retailer_group_user_invite_accepted.txt"
+        )
+
+
+class RetailerGroupUserApprovedNotificationEmail(TemplateEmailBase):
+    def __init__(self, SYSTEM_NAME, retailer_group_name):
+        super().__init__()
+        self.subject = f"Your invite to the { SYSTEM_NAME } Retailer Group {retailer_group_name} has been Approved"
+        self.html_template = (
+            "parkpasses/emails/retailer/retailer_group_user_invite_approved.html"
+        )
+        self.txt_template = (
+            "parkpasses/emails/retailer/retailer_group_user_invite_approved.txt"
+        )
+
+
+class RetailerGroupUserDeniedNotificationEmail(TemplateEmailBase):
+    def __init__(self, SYSTEM_NAME, retailer_group_name):
+        super().__init__()
+        self.subject = f"Your invite to the { SYSTEM_NAME } Retailer Group {retailer_group_name} has been Denied"
+        self.html_template = (
+            "parkpasses/emails/retailer/retailer_group_user_invite_denied.html"
+        )
+        self.txt_template = (
+            "parkpasses/emails/retailer/retailer_group_user_invite_denied.txt"
+        )
 
 
 class RetailerEmails:
@@ -28,24 +62,114 @@ class RetailerEmails:
         self, retailer_group_user_invite
     ):
         email = RetailerGroupUserInviteNotificationEmail(
-            retailer_group_user_invite.retailer_group_name
+            settings.SYSTEM_NAME, retailer_group_user_invite.retailer_group__name
         )
         context = {
             "SYSTEM_NAME": settings.SYSTEM_NAME,
             "retailer_group_user_invite": retailer_group_user_invite,
         }
         message = email.send(retailer_group_user_invite.email, context=context)
-        content_type = ContentType.objects.get_for_model(retailer_group_user_invite)
-        entry_type = EntryType.objects.get(entry_type__iexact="email")
-        staff = EmailUser.objects.get(email__icontains=settings.DEFAULT_FROM_EMAIL)
-        communication_log_kwargs = {
-            "content_type": content_type,
-            "object_id": str(retailer_group_user_invite.id),
-            "to": retailer_group_user_invite.email,
-            "fromm": settings.DEFAULT_FROM_EMAIL,
-            "entry_type": entry_type,
-            "subject": message.subject,
-            "text": message.body,
-            "staff": staff.id,
+        if message:
+            content_type = ContentType.objects.get_for_model(retailer_group_user_invite)
+            entry_type = EntryType.objects.get(entry_type__iexact="email")
+            staff = EmailUser.objects.get(email__icontains=settings.DEFAULT_FROM_EMAIL)
+            communication_log_kwargs = {
+                "content_type": content_type,
+                "object_id": str(retailer_group_user_invite.id),
+                "to": retailer_group_user_invite.email,
+                "fromm": settings.DEFAULT_FROM_EMAIL,
+                "entry_type": entry_type,
+                "subject": message.subject,
+                "text": message.body,
+                "staff": staff.id,
+            }
+            CommunicationsLogEntry.objects.log_communication(**communication_log_kwargs)
+        return message
+
+    def send_retailer_group_user_accepted_notification_email(
+        self, retailer_group_user_invite
+    ):
+        email = RetailerGroupUserAcceptedNotificationEmail(
+            retailer_group_user_invite.email,
+            retailer_group_user_invite.retailer_group__name,
+        )
+        context = {
+            "ADMIN_GROUP": settings.ADMIN_GROUP,
+            "retailer_group_user_invite": retailer_group_user_invite,
         }
-        CommunicationsLogEntry.objects.log_communication(**communication_log_kwargs)
+        message = email.send(retailer_group_user_invite.email, context=context)
+        if message:
+            content_type = ContentType.objects.get_for_model(retailer_group_user_invite)
+            entry_type = EntryType.objects.get(entry_type__iexact="email")
+            staff = EmailUser.objects.get(email__icontains=settings.DEFAULT_FROM_EMAIL)
+            communication_log_kwargs = {
+                "content_type": content_type,
+                "object_id": str(retailer_group_user_invite.id),
+                "to": retailer_group_user_invite.email,
+                "fromm": settings.DEFAULT_FROM_EMAIL,
+                "entry_type": entry_type,
+                "subject": message.subject,
+                "text": message.body,
+                "customer": retailer_group_user_invite.user,
+                "staff": staff.id,
+            }
+            CommunicationsLogEntry.objects.log_communication(**communication_log_kwargs)
+        return message
+
+    def send_retailer_group_user_approved_notification_email(
+        self, retailer_group_user_invite
+    ):
+        email = RetailerGroupUserApprovedNotificationEmail(
+            settings.SYSTEM_NAME, retailer_group_user_invite.retailer_group__name
+        )
+        context = {
+            "SYSTEM_NAME": settings.SYSTEM_NAME,
+            "retailer_group_user_invite": retailer_group_user_invite,
+        }
+        message = email.send(retailer_group_user_invite.email, context=context)
+        if message:
+            content_type = ContentType.objects.get_for_model(retailer_group_user_invite)
+            entry_type = EntryType.objects.get(entry_type__iexact="email")
+            staff = EmailUser.objects.get(email__icontains=settings.DEFAULT_FROM_EMAIL)
+            communication_log_kwargs = {
+                "content_type": content_type,
+                "object_id": str(retailer_group_user_invite.id),
+                "to": retailer_group_user_invite.email,
+                "fromm": settings.DEFAULT_FROM_EMAIL,
+                "entry_type": entry_type,
+                "subject": message.subject,
+                "text": message.body,
+                "customer": retailer_group_user_invite.user,
+                "staff": staff.id,
+            }
+            CommunicationsLogEntry.objects.log_communication(**communication_log_kwargs)
+            return message
+
+    def send_retailer_group_user_denied_notification_email(
+        self, retailer_group_user_invite
+    ):
+        email = RetailerGroupUserDeniedNotificationEmail(
+            settings.SYSTEM_NAME, retailer_group_user_invite.retailer_group__name
+        )
+        context = {
+            "SYSTEM_NAME": settings.SYSTEM_NAME,
+            "retailer_group_user_invite": retailer_group_user_invite,
+        }
+        message = email.send(retailer_group_user_invite.email, context=context)
+        if message:
+            content_type = ContentType.objects.get_for_model(retailer_group_user_invite)
+            entry_type = EntryType.objects.get(entry_type__iexact="email")
+            staff = EmailUser.objects.get(email__icontains=settings.DEFAULT_FROM_EMAIL)
+            communication_log_kwargs = {
+                "content_type": content_type,
+                "object_id": str(retailer_group_user_invite.id),
+                "to": retailer_group_user_invite.email,
+                "fromm": settings.DEFAULT_FROM_EMAIL,
+                "entry_type": entry_type,
+                "subject": message.subject,
+                "text": message.body,
+                "customer": retailer_group_user_invite.user,
+                "staff": staff.id,
+            }
+            CommunicationsLogEntry.objects.log_communication(**communication_log_kwargs)
+        return message
