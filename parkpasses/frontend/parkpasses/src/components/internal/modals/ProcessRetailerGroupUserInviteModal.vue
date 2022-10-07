d55709e3d446eb58dccdcacc64e2d48d338ca2aa
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-07 15:53:50 +0800

    Made sure is_admin is reactive.

--- parkpasses/frontend/parkpasses/src/components/internal/modals/ProcessRetailerGroupUserInviteModal.vue
+++ parkpasses/frontend/parkpasses/src/components/internal/modals/ProcessRetailerGroupUserInviteModal.vue
@@ -28,12 +28,12 @@
                         </div>
                         <div class="form-check form-switch">
                             <label for="makeAdmin" class="form-check-label">Make this User an Admin</label>
-                            <input class="form-check-input" type="checkbox" id="makeAdmin" name="makeAdmin" :checked="getAdminDefault" :disabled="getAdminDefault"  aria-describedby="makeAdminHelpBlock">
+                            <input class="form-check-input" type="checkbox" id="makeAdmin" name="makeAdmin" v-model="approval.is_admin" :checked="getAdminDefault" :disabled="getAdminDefault"  aria-describedby="makeAdminHelpBlock">
                         </div>
                         <div id="makeAdminHelpBlock" class="form-text">
                             Admin users can invite other users to their retailer group.
                         </div>
-                        <div class="alert alert-warning d-flex align-items-center mt-3" role="alert">
+                        <div v-if="!retailerGroupUserInvite.user_count_for_retailer_group" class="alert alert-warning d-flex align-items-center mt-3" role="alert">
                             <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
                                 <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
                                     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
@@ -44,7 +44,6 @@
                                 The first user added to a group must be an admin.
                             </div>
                         </div>
-
                     </div>
                     <div class="modal-footer">
                         <template v-if="loading">
@@ -108,8 +107,6 @@ export default {
             vm.loading = true;
             vm.approval.csrfmiddlewaretoken = helpers.getCookie('csrftoken');
             vm.approval.approved = approved;
-            vm.approval.is_admin = vm.getAdminDefault;
-            alert(JSON.stringify(vm.approval));
             const requestOptions = {
                 method: "PUT",
                 headers: { "Content-Type": "application/json" },
