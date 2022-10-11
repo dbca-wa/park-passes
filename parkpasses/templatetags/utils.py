ab3079b91342f8491942041093325db99278028e
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-11 16:24:13 +0800

    Removed unused tags.

--- parkpasses/templatetags/utils.py
+++ parkpasses/templatetags/utils.py
@@ -34,19 +34,6 @@ def can_show_tests():
     return settings.SHOW_TESTS_URL
 
 
-@register.filter
-def total_line_price(price, qty):
-    return f"{round(price * qty, 2):.2f}"
-
-
-@register.filter
-def basket_total_price(lines):
-    total = 0.00
-    for line in lines:
-        total += line["price_incl_tax"] * line["quantity"]
-    return f"{round(total, 2):.2f}"
-
-
 @register.simple_tag()
 def build_tag():
     return settings.BUILD_TAG
