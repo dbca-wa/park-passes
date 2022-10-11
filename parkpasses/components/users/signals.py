3685d6256c54bbc63ea31106c0359c1ba1a18cc0
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-11 16:29:23 +0800

    Added signal to add retailer info into session when needed.

--- parkpasses/components/users/signals.py
+++ parkpasses/components/users/signals.py
@@ -2,6 +2,8 @@ from django.contrib.auth.signals import user_logged_in
 
 from parkpasses.components.cart.models import Cart
 from parkpasses.components.passes.models import Pass
+from parkpasses.components.retailers.models import RetailerGroupUser
+from parkpasses.helpers import is_retailer
 
 
 def init_cart(sender, user, request, **kwargs):
@@ -9,6 +11,21 @@ def init_cart(sender, user, request, **kwargs):
     we need to attach the correct user to those items once they log in"""
     cart = Cart.get_or_create_cart(request)
     cart.set_user_for_cart_and_items(request.user.id)
+
+
+def add_retailer_to_session(sender, user, request, **kwargs):
+    if is_retailer(request):
+        retailer_group_user = (
+            RetailerGroupUser.objects.filter(emailuser=request.user.id)
+            .order_by("-datetime_created")
+            .first()
+        )
+        request.session["retailer"] = {
+            "id": retailer_group_user.retailer_group.id,
+            "name": retailer_group_user.retailer_group.name,
+        }
+    else:
+        del request.session["retailer"]
 
 
 def assign_orphan_passes(sender, user, request, **kwargs):
@@ -24,3 +41,4 @@ def assign_orphan_passes(sender, user, request, **kwargs):
 
 user_logged_in.connect(init_cart)
 user_logged_in.connect(assign_orphan_passes)
+user_logged_in.connect(add_retailer_to_session)
