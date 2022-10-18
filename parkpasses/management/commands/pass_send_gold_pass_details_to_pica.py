249fc9198c23d399a42c405113f269bdbb649a16
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-18 08:29:39 +0800

    Added new management command to send any new gold star pass details to pica.

---
+++ parkpasses/management/commands/pass_send_gold_pass_details_to_pica.py
@@ -0,0 +1,136 @@
+"""
+This management commands sends the details of any new gold star passes to pica so they
+    can arrange for their landscope magazine subscription to be sent out.
+
+Usage: ./manage.sh pass_send_gold_pass_details_to_pica
+        (this command should be run by a cron job at 3am each day)
+
+"""
+import logging
+from pathlib import Path
+
+import xlsxwriter
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from django.utils.text import slugify
+from ledger_api_client.ledger_models import EmailUserRO as EmailUser
+
+from parkpasses.components.passes.models import Pass
+
+logger = logging.getLogger(__name__)
+
+
+def get_col_widths(dataframe):
+    # First we find the maximum length of the index column
+    idx_max = max(
+        [len(str(s)) for s in dataframe.index.values] + [len(str(dataframe.index.name))]
+    )
+    # Then, we concatenate this to the max of the lengths of column name and its values for each column, left to right
+    return [idx_max] + [
+        max([len(str(s)) for s in dataframe[col].values] + [len(col)])
+        for col in dataframe.columns
+    ]
+
+
+class Command(BaseCommand):
+    help = "Sends the details of any new gold star passes to pica."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--test",
+            action="store_true",
+            help="Adding the test flag will output what emails would be sent without actually sending them.",
+        )
+
+    def handle(self, *args, **options):
+        no_reply_email_user, created = EmailUser.objects.get_or_create(
+            email=settings.NO_REPLY_EMAIL, password=""
+        )
+        today = timezone.now().date()
+        yesterday = today - timezone.timedelta(days=1)
+        # Don't bother doing more expensive query if there are no passes that satisfy the basic criteria
+        pass_queryset = Pass.objects.exclude(
+            cancellation__isnull=False,  # to exclude cancelled passes
+        ).filter(
+            in_cart=False,
+            option__pricing_window__pass_type__name=settings.GOLD_STAR_PASS,
+        )
+        if options["test"]:
+            # It's handy for testing to be able to use passes created today rather than yesterday
+            pass_queryset = pass_queryset.filter(
+                datetime_created__date=today,
+            )
+        else:
+            pass_queryset = pass_queryset.filter(
+                datetime_created__date=yesterday,
+            )
+
+        if pass_queryset.exists():
+            passes = pass_queryset
+            if options["test"]:
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Found {len(passes)} gold star park pass(es) that were sold today."
+                    )
+                )
+            else:
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Found {len(passes)} gold star park pass(es) that were sold yesterday."
+                    )
+                )
+
+            Path(settings.PICA_GOLD_STAR_PASS_ROOT).mkdir(parents=True, exist_ok=True)
+
+            file_path = (
+                settings.PICA_GOLD_STAR_PASS_ROOT
+                + f"/gold-star-pass-sales-for-pica-{slugify(yesterday)}.xlsx"
+            )
+            workbook = xlsxwriter.Workbook(file_path)
+            worksheet = workbook.add_worksheet()
+
+            bold = workbook.add_format({"bold": True})
+
+            worksheet.write("A1", "Pass Number", bold)
+            worksheet.write("B1", "First Name", bold)
+            worksheet.write("C1", "Last Name", bold)
+            worksheet.write("D1", "Address Line 1", bold)
+            worksheet.write("E1", "Address Line 2", bold)
+            worksheet.write("F1", "Suburb", bold)
+            worksheet.write("G1", "Postcode", bold)
+            worksheet.write("H1", "State", bold)
+            worksheet.write("I1", "Mobile", bold)
+            worksheet.write("J1", "Start Date of Pass", bold)
+
+            worksheet.set_column(0, 2, 15)
+            worksheet.set_column(3, 4, 30)
+            worksheet.set_column(5, 9, 15)
+
+            for row_num, data in enumerate(passes):
+                worksheet.write(row_num + 1, 0, data.pass_number)
+                worksheet.write(row_num + 1, 1, data.first_name)
+                worksheet.write(row_num + 1, 2, data.last_name)
+                worksheet.write(row_num + 1, 3, data.address_line_1)
+                worksheet.write(row_num + 1, 4, data.address_line_2)
+                worksheet.write(row_num + 1, 5, data.suburb)
+                worksheet.write(row_num + 1, 6, data.postcode)
+                worksheet.write(row_num + 1, 7, data.state)
+                worksheet.write(row_num + 1, 8, data.mobile)
+                worksheet.write(row_num + 1, 9, data.date_start.strftime("%d-%m-%Y"))
+
+            workbook.close()
+
+            for park_pass in passes:
+
+                if options["test"]:
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f"TEST: pretending to call send_gold_pass_details_to_pica on Pass: {park_pass}"
+                        )
+                    )
+                else:
+                    park_pass.send_gold_pass_details_to_pica(file_path)
+                    logger.info(
+                        f"Notification email sent to recipient of Pass: {park_pass}"
+                    )
