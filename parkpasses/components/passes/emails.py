aa83c42a36aaf649bf244c1db61e06478d60482a
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-18 08:34:44 +0800

    Added send_gold_pass_details_to_pica method.

--- parkpasses/components/passes/emails.py
+++ parkpasses/components/passes/emails.py
@@ -10,37 +10,84 @@ logger = logging.getLogger(__name__)
 
 
 class PassAutoRenewNotificationEmail(TemplateEmailBase):
-    subject = "Park Pass AutoRenewal Notification"
-    html_template = "parkpasses/emails/pass_autorenew_notification.html"
-    txt_template = "parkpasses/emails/pass_autorenew_notification.txt"
+    def __init__(self):
+        super().__init__()
+        self.subject = "Park Pass AutoRenewal Notification"
+        self.html_template = "parkpasses/emails/pass_autorenew_notification.html"
+        self.txt_template = "parkpasses/emails/pass_autorenew_notification.txt"
 
 
 class PassExpiryNotificationEmail(TemplateEmailBase):
-    subject = "Your Park Pass is Expiring Soon"
-    html_template = "parkpasses/emails/pass_expiry_notification.html"
-    txt_template = "parkpasses/emails/pass_expiry_notification.txt"
+    def __init__(self):
+        super().__init__()
+        self.subject = "Your Park Pass is Expiring Soon"
+        self.html_template = "parkpasses/emails/pass_expiry_notification.html"
+        self.txt_template = "parkpasses/emails/pass_expiry_notification.txt"
 
 
 class PassPurchasedNotificationEmail(TemplateEmailBase):
-    subject = "Your Park Pass is Ready"
-    html_template = "parkpasses/emails/pass_purchased_notification.html"
-    txt_template = "parkpasses/emails/pass_purchased_notification.txt"
+    def __init__(self):
+        self.subject = "Your Park Pass is Ready"
+        self.html_template = "parkpasses/emails/pass_purchased_notification.html"
+        self.txt_template = "parkpasses/emails/pass_purchased_notification.txt"
+
+
+class PassUpdatedNotificationEmail(TemplateEmailBase):
+    def __init__(self):
+        self.subject = "Your Park Pass has Been Updated"
+        self.html_template = "parkpasses/emails/pass_updated_notification.html"
+        self.txt_template = "parkpasses/emails/pass_updated_notification.txt"
 
 
 class PassVehicleDetailsNotYetProvidedNotificationEmail(TemplateEmailBase):
-    subject = "Your Park Pass still has no Vehicle Registration Numbers Attached"
-    html_template = (
-        "parkpasses/emails/pass_vehicle_details_not_yet_provided_notification.html"
-    )
-    txt_template = (
-        "parkpasses/emails/pass_vehicle_details_not_yet_provided_notification.txt"
-    )
+    def __init__(self):
+        self.subject = (
+            "Your Park Pass still has no Vehicle Registration Numbers Attached"
+        )
+        self.html_template = (
+            "parkpasses/emails/pass_vehicle_details_not_yet_provided_notification.html"
+        )
+        self.txt_template = (
+            "parkpasses/emails/pass_vehicle_details_not_yet_provided_notification.txt"
+        )
+
+
+class PassGoldPassDetailsForPICAEmail(TemplateEmailBase):
+    def __init__(self, date_sold):
+        super().__init__()
+        self.subject = f"Gold Star Park Passes Sold [Yesterday: {date_sold}]"
+        self.html_template = "parkpasses/emails/pass_autorenew_notification.html"
+        self.txt_template = "parkpasses/emails/pass_autorenew_notification.txt"
 
 
 class PassEmails:
     @classmethod
-    def send_pass_autorenew_notification_email(self, park_pass):
-        email = PassAutoRenewNotificationEmail()
+    def send_pass_purchased_notification_email(self, park_pass):
+        email = PassPurchasedNotificationEmail()
+        ExternalPassSerializer = apps.get_model("parkpasses", "ExternalPassSerializer")
+        serializer = ExternalPassSerializer(park_pass)
+        context = {
+            "pass": serializer.data,
+            "site_url": settings.SITE_URL,
+        }
+        message = email.send(park_pass.email, context=context)
+        log_communication(park_pass.recipient_email, message, "email", park_pass)
+
+    @classmethod
+    def send_pass_updated_notification_email(self, park_pass):
+        email = PassUpdatedNotificationEmail()
+        ExternalPassSerializer = apps.get_model("parkpasses", "ExternalPassSerializer")
+        serializer = ExternalPassSerializer(park_pass)
+        context = {
+            "pass": serializer.data,
+            "site_url": settings.SITE_URL,
+        }
+        message = email.send(park_pass.email, context=context)
+        log_communication(park_pass.recipient_email, message, "email", park_pass)
+
+    @classmethod
+    def send_pass_vehicle_details_not_yet_provided_notification_email(self, park_pass):
+        email = PassVehicleDetailsNotYetProvidedNotificationEmail()
         ExternalPassSerializer = apps.get_model("parkpasses", "ExternalPassSerializer")
         serializer = ExternalPassSerializer(park_pass)
         context = {
@@ -63,8 +110,8 @@ class PassEmails:
         log_communication(park_pass.recipient_email, message, "email", park_pass)
 
     @classmethod
-    def send_pass_purchased_notification_email(self, park_pass):
-        email = PassPurchasedNotificationEmail()
+    def send_pass_autorenew_notification_email(self, park_pass):
+        email = PassAutoRenewNotificationEmail()
         ExternalPassSerializer = apps.get_model("parkpasses", "ExternalPassSerializer")
         serializer = ExternalPassSerializer(park_pass)
         context = {
@@ -75,13 +122,17 @@ class PassEmails:
         log_communication(park_pass.recipient_email, message, "email", park_pass)
 
     @classmethod
-    def send_pass_vehicle_details_not_yet_provided_notification_email(self, park_pass):
-        email = PassVehicleDetailsNotYetProvidedNotificationEmail()
+    def send_gold_pass_details_to_pica(self, park_pass, gold_passes_excel_file):
+        email = PassAutoRenewNotificationEmail(
+            park_pass.datetime_created.strftime("%d/%m/%Y")
+        )
         ExternalPassSerializer = apps.get_model("parkpasses", "ExternalPassSerializer")
         serializer = ExternalPassSerializer(park_pass)
         context = {
             "pass": serializer.data,
             "site_url": settings.SITE_URL,
         }
-        message = email.send(park_pass.email, context=context)
+        message = email.send(
+            park_pass.email, context=context, attachments=[gold_passes_excel_file]
+        )
         log_communication(park_pass.recipient_email, message, "email", park_pass)
