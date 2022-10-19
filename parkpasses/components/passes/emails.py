484caa8387956df164c4cbc2e5be7b9b8f612d2b
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-19 16:45:55 +0800

    Added attachments to required pass emails.

--- parkpasses/components/passes/emails.py
+++ parkpasses/components/passes/emails.py
@@ -1,6 +1,6 @@
 import logging
+import os
 
-from django.apps import apps
 from django.conf import settings
 
 from parkpasses.components.emails.emails import TemplateEmailBase
@@ -56,71 +56,92 @@ class PassGoldPassDetailsForPICAEmail(TemplateEmailBase):
     def __init__(self, date_sold):
         super().__init__()
         self.subject = f"Gold Star Park Passes Sold [Yesterday: {date_sold}]"
-        self.html_template = "parkpasses/emails/pass_autorenew_notification.html"
-        self.txt_template = "parkpasses/emails/pass_autorenew_notification.txt"
+        self.html_template = (
+            "parkpasses/emails/pass_send_gold_pass_details_to_pica.html"
+        )
+        self.txt_template = "parkpasses/emails/pass_send_gold_pass_details_to_pica.txt"
 
 
 class PassEmails:
     @classmethod
     def send_pass_purchased_notification_email(self, park_pass):
         email = PassPurchasedNotificationEmail()
-        ExternalPassSerializer = apps.get_model("parkpasses", "ExternalPassSerializer")
+        from parkpasses.components.passes.serializers import ExternalPassSerializer
+
         serializer = ExternalPassSerializer(park_pass)
         context = {
             "pass": serializer.data,
             "site_url": settings.SITE_URL,
         }
-        message = email.send(park_pass.email, context=context)
-        log_communication(park_pass.recipient_email, message, "email", park_pass)
+        attachments = []
+        park_pass_pdf_file = park_pass.park_pass_pdf.open()
+        file_name = os.path.basename(park_pass_pdf_file.name)
+        attachment = (file_name, park_pass_pdf_file.read(), "application/pdf")
+        attachments.append(attachment)
+        message = email.send(park_pass.email, context=context, attachments=attachments)
+        log_communication(park_pass.email, message, "email", park_pass)
 
     @classmethod
     def send_pass_updated_notification_email(self, park_pass):
         email = PassUpdatedNotificationEmail()
-        ExternalPassSerializer = apps.get_model("parkpasses", "ExternalPassSerializer")
+        from parkpasses.components.passes.serializers import ExternalPassSerializer
+
         serializer = ExternalPassSerializer(park_pass)
         context = {
             "pass": serializer.data,
             "site_url": settings.SITE_URL,
         }
-        message = email.send(park_pass.email, context=context)
-        log_communication(park_pass.recipient_email, message, "email", park_pass)
+        attachments = []
+        park_pass_pdf_file = park_pass.park_pass_pdf.open()
+        file_name = os.path.basename(park_pass_pdf_file.name)
+        attachment = (file_name, park_pass_pdf_file.read(), "application/pdf")
+        attachments.append(attachment)
+        message = email.send(park_pass.email, context=context, attachments=attachments)
+        log_communication(park_pass.email, message, "email", park_pass)
 
     @classmethod
     def send_pass_vehicle_details_not_yet_provided_notification_email(self, park_pass):
         email = PassVehicleDetailsNotYetProvidedNotificationEmail()
-        ExternalPassSerializer = apps.get_model("parkpasses", "ExternalPassSerializer")
+        from parkpasses.components.passes.serializers import ExternalPassSerializer
+
         serializer = ExternalPassSerializer(park_pass)
         context = {
             "pass": serializer.data,
             "site_url": settings.SITE_URL,
         }
         message = email.send(park_pass.email, context=context)
-        log_communication(park_pass.recipient_email, message, "email", park_pass)
+        log_communication(park_pass.email, message, "email", park_pass)
 
     @classmethod
     def send_pass_expiry_notification_email(self, park_pass):
         email = PassExpiryNotificationEmail()
-        ExternalPassSerializer = apps.get_model("parkpasses", "ExternalPassSerializer")
+        from parkpasses.components.passes.serializers import ExternalPassSerializer
+
         serializer = ExternalPassSerializer(park_pass)
         context = {
             "pass": serializer.data,
             "site_url": settings.SITE_URL,
         }
         message = email.send(park_pass.email, context=context)
-        log_communication(park_pass.recipient_email, message, "email", park_pass)
+        log_communication(park_pass.email, message, "email", park_pass)
 
     @classmethod
-    def send_gold_pass_details_to_pica(self, park_pass, gold_passes_excel_file):
+    def send_gold_pass_details_to_pica(self, date, passes, gold_passes_excel_file_path):
         logger.debug("send_gold_pass_details_to_pica")
-        email = PassGoldPassDetailsForPICAEmail(
-            park_pass.datetime_created.strftime("%d/%m/%Y")
-        )
+        email = PassGoldPassDetailsForPICAEmail(date.strftime("%d/%m/%Y"))
         logger.debug(str(email))
         context = {
-            "park_pass": park_pass,
+            "passes": passes,
             "site_url": settings.SITE_URL,
         }
-        message = email.send(
-            park_pass.email, context=context, attachments=[gold_passes_excel_file]
+        attachments = []
+        content = open(gold_passes_excel_file_path, "rb").read()
+        file_name = os.path.basename(gold_passes_excel_file_path)
+        attachment = (
+            file_name,
+            content,
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         )
-        log_communication(park_pass.recipient_email, message, "email", park_pass)
+        attachments.append(attachment)
+        email.send(settings.PICA_EMAIL, context=context, attachments=attachments)
+        logger.debug("Email sent supposedly.")
