a70aa437e4697120fb2514c38a31f1f1da01dfd8
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-19 16:38:31 +0800

    Standardised cache key.

--- parkpasses/ledger_api_utils.py
+++ parkpasses/ledger_api_utils.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.conf import settings
 from django.core.cache import cache
 from ledger_api_client.ledger_models import EmailUserRO as EmailUser
 
@@ -10,9 +11,10 @@ logger = logging.getLogger(__name__)
 
 @basic_exception_handler
 def retrieve_email_user(email_user_id):
-    emailuser = cache.get("email_user_" + str(email_user_id))
+    cache_key = settings.CACHE_KEY_EMAIL_USER.format(str(email_user_id))
+    emailuser = cache.get(cache_key)
     if emailuser is None:
         emailuser = EmailUser.objects.get(id=email_user_id)
-        cache.set("email_user_" + str(email_user_id), emailuser, 300)
+        cache.set(cache_key, emailuser, 300)
         return emailuser
     return emailuser
