be96b96cd773b7c4ead5ee4f0674995edaee8dd8
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-19 16:49:15 +0800

    Reworked the IsInternal permission checking to disallow destroy action to all users and to allow remaining groups to do appropriate actions.

--- parkpasses/permissions.py
+++ parkpasses/permissions.py
@@ -1,18 +1,33 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
-from parkpasses.helpers import is_internal, is_retailer, is_retailer_admin
+from parkpasses.helpers import (
+    is_internal,
+    is_parkpasses_officer,
+    is_parkpasses_payments_officer,
+    is_retailer,
+    is_retailer_admin,
+)
 
 
 class IsInternal(BasePermission):
     def has_permission(self, request, view):
-        return is_internal(request)
+        if is_internal(request):
+            if view.action in ["destroy"]:
+                return False
+            if view.action in ["retrieve", "list"]:
+                return True
+            if (
+                request.user.is_superuser
+                or is_parkpasses_payments_officer(request)
+                or is_parkpasses_officer(request)
+            ):
+                return True
+        return False
 
 
 class IsInternalOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.method in SAFE_METHODS or request.user and is_internal(request)
-        )
+        return bool(request.method in SAFE_METHODS or is_internal(request))
 
 
 class IsRetailer(BasePermission):
