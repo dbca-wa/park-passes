6f572f1ef749d96d27b463d6c6c4a93f41a629c4
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-26 16:48:47 +0800

    Simplified access to user variable.

--- parkpasses/components/users/signals.py
+++ parkpasses/components/users/signals.py
@@ -10,13 +10,13 @@ def init_cart(sender, user, request, **kwargs):
     """Since items can be added to a cart anonymously
     we need to attach the correct user to those items once they log in"""
     cart = Cart.get_or_create_cart(request)
-    cart.set_user_for_cart_and_items(request.user.id)
+    cart.set_user_for_cart_and_items(user.id)
 
 
 def add_retailer_to_session(sender, user, request, **kwargs):
     if is_retailer(request):
         retailer_group_user = (
-            RetailerGroupUser.objects.filter(emailuser=request.user.id)
+            RetailerGroupUser.objects.filter(emailuser=user.id)
             .order_by("-datetime_created")
             .first()
         )
@@ -32,11 +32,10 @@ def add_retailer_to_session(sender, user, request, **kwargs):
 def assign_orphan_passes(sender, user, request, **kwargs):
     """When passes are sold via retailers, the user doesn't necessarily have an account in ledger
     so we need a way to attach the user to those passes when they log in"""
-    email_user = request.user
-    queryset = Pass.objects.filter(user__isnull=True, email=email_user.email)
+    queryset = Pass.objects.filter(user__isnull=True, email=user.email)
     if queryset.exists():
         for park_pass in queryset:
-            park_pass.user = email_user.id
+            park_pass.user = user.id
             park_pass.save()
 
 
