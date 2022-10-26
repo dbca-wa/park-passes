5f9e46200cfc0a0c704bde59dc4a0e2675aa99e4
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-26 10:47:51 +0800

    Added centerofScreen prop so the developer can center the spinner in the component instead if they wish.

--- parkpasses/frontend/parkpasses/src/utils/vue/BootstrapSpinner.vue
+++ parkpasses/frontend/parkpasses/src/utils/vue/BootstrapSpinner.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="position-absolute w-100 h-100 d-flex flex-column align-items-center bg-white justify-content-center">
-    <div class="spinner spinner-border org-primary" style="width: 3rem; height: 3rem;" role="status">
+  <div v-if="centerOfScreen" class="position-absolute w-100 h-100 d-flex flex-column align-items-center bg-white justify-content-center">
+    <div :class="spinnerClass" class="spinner spinner-border org-primary" style="width: 3rem; height: 3rem;" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+  <div v-else>
+    <div :class="spinnerClass" class="spinner-border org-primary" style="width: 3rem; height: 3rem;" role="status">
       <span class="visually-hidden">Loading...</span>
     </div>
   </div>
@@ -11,16 +16,30 @@ export default {
   name:'BootstrapSpinner',
   props:{
       isLoading:{
-          default:function () {
-              return false;
-          }
+          default: false
+      },
+      centerOfScreen:{
+          type: Boolean,
+          default: true
       }
+  },
+  data: function () {
+    return {
+      spinnerClass: '',
+    };
+  },
+  created: function () {
+    if(!this.centerOfScreen){
+      this.spinnerClass = 'spinner-center-component';
+    } else {
+      this.spinnerClass = 'spinner-center-screen';
+    }
   }
 }
 </script>
 
 <style lang="css">
-  .spinner {
+  .spinner-center-screen {
     display: block;
     position: fixed;
     z-index: 1031; /* High z-index so it is on top of the page */
@@ -28,5 +47,10 @@ export default {
     right: 50%; /* or: left: 50%; */
     /*margin-top: -..px;  half of the elements height */
    /* margin-right: -..px;  half of the elements width */
+  }
+
+  .spinner-center-component {
+    margin-top:50px;
+    margin-left:140px;
   }
 </style>
