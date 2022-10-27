593255de6537d07d72ef05465f9d6b49eedd7fcc
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-27 14:35:20 +0800

    Added code to show Anonymous User if the id stored on the user action is 0. Passes created before the user has logged in will be logged with 0 as the customer id.

--- parkpasses/components/main/serializers.py
+++ parkpasses/components/main/serializers.py
@@ -1,20 +1,20 @@
 from django.contrib.contenttypes.models import ContentType
 from org_model_documents.models import Document
 from org_model_documents.serializers import DocumentSerializer
-from org_model_logs.serializers import (
-    CommunicationsLogEntrySerializer,
-    UserActionSerializer,
-)
 from rest_framework import serializers
 
+from org_model_logs.serializers import BaseCommunicationsLogEntrySerializer
+from org_model_logs.serializers import UserActionSerializer as BaseUserActionSerializer
 from parkpasses.ledger_api_utils import retrieve_email_user
 
 
-class UserActionSerializer(UserActionSerializer):
+class UserActionSerializer(BaseUserActionSerializer):
     who = serializers.SerializerMethodField()
     documents = serializers.SerializerMethodField()
 
     def get_who(self, obj):
+        if 0 == obj.who:
+            return "Anonymous User"
         return retrieve_email_user(obj.who).get_full_name()
 
     def get_documents(self, obj):
@@ -30,7 +30,7 @@ class UserActionSerializer(UserActionSerializer):
         return documents
 
 
-class CommunicationsLogEntrySerializer(CommunicationsLogEntrySerializer):
+class CommunicationsLogEntrySerializer(BaseCommunicationsLogEntrySerializer):
     documents = serializers.SerializerMethodField()
 
     def get_documents(self, obj):
