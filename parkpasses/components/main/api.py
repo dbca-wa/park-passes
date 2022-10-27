8cd9f20933f3e13455504b44ceb16babea8477f8
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-10-27 14:31:08 +0800

    Renamed view and viewset imports to avoid confusion.

--- parkpasses/components/main/api.py
+++ parkpasses/components/main/api.py
@@ -1,19 +1,19 @@
 import logging
 
 from org_model_documents.api import DocumentCreateView, DocumentViewSet
-from org_model_logs.api import (
-    CreateCommunicationsLogEntry,
-    EntryTypeList,
-    UserActionList,
-    UserActionViewSet,
-)
-from org_model_logs.serializers import EntryTypeSerializer
 
+from org_model_logs.api import (
+    CreateCommunicationsLogEntry as BaseCreateCommunicationsLogEntry,
+)
+from org_model_logs.api import EntryTypeList as BaseEntryTypeList
+from org_model_logs.api import UserActionList as BaseUserActionList
+from org_model_logs.api import UserActionViewSet as BaseUserActionViewSet
+from org_model_logs.serializers import EntryTypeSerializer
 from parkpasses.components.main.serializers import (
     CommunicationsLogEntrySerializer,
     UserActionSerializer,
 )
-from parkpasses.permissions import IsInternal
+from parkpasses.permissions import IsInternal, IsInternalAPIView
 
 logger = logging.getLogger(__name__)
 
@@ -23,29 +23,29 @@ so can't be included in the org_model_documents or org_model_logs apps as it wou
 
 
 class DocumentCreateView(DocumentCreateView):
-    permission_classes = [IsInternal]
+    permission_classes = [IsInternalAPIView]
 
 
 class DocumentViewSet(DocumentViewSet):
     permission_classes = [IsInternal]
 
 
-class EntryTypeList(EntryTypeList):
+class EntryTypeList(BaseEntryTypeList):
     serializer_class = EntryTypeSerializer
-    permission_classes = [IsInternal]
+    permission_classes = [IsInternalAPIView]
 
 
-class UserActionList(UserActionList):
+class UserActionList(BaseUserActionList):
     serializer_class = UserActionSerializer
+    permission_classes = [IsInternalAPIView]
+
+
+class UserActionViewSet(BaseUserActionViewSet):
     permission_classes = [IsInternal]
 
 
-class UserActionViewSet(UserActionViewSet):
-    permission_classes = [IsInternal]
-
-
-class CreateCommunicationsLogEntry(CreateCommunicationsLogEntry):
-    permission_classes = [IsInternal]
+class CreateCommunicationsLogEntry(BaseCreateCommunicationsLogEntry):
+    permission_classes = [IsInternalAPIView]
 
     def get_serializer_class(self):
         logger.debug("self.request.method = " + str(self.request.method))
