7aeea81ad744b5191eb97011c66dac6290f63ccb
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-11-08 12:38:03 +0800

    Corrected urls.

--- parkpasses/components/main/urls.py
+++ parkpasses/components/main/urls.py
@@ -16,26 +16,27 @@ router.register(
     basename="org-model-documents-internal",
 )
 urlpatterns = [
+    # ========================================================================== Org Model Logs
     url(
-        r"api/main/org-model-logs/user-actions",
+        r"org-model-logs/user-actions",
         UserActionList.as_view(),
         name="user-actions",
     ),
     url(
-        r"api/main/internal/org-model-documents/upload-documents",
-        DocumentCreateView.as_view(),
-        name="upload-documents",
-    ),
-    # ========================================================================== Org Model Logs
-    url(
-        r"api/main/org-model-logs/entry-types",
+        r"org-model-logs/entry-types",
         EntryTypeList.as_view(),
         name="entry-types",
     ),
     url(
-        r"api/main/internal/org-model-logs/communications-log-entries",
+        r"internal/org-model-logs/communications-log-entries",
         ListCreateCommunicationsLogEntry.as_view(),
         name="list-create-communications-log-entries",
+    ),
+    # ========================================================================== Org Model Documents
+    url(
+        r"internal/org-model-documents/upload-documents",
+        DocumentCreateView.as_view(),
+        name="upload-documents",
     ),
 ]
 
