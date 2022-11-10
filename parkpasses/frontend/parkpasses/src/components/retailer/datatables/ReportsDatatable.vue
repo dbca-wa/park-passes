dd232bdc5e67fc6888c7b6f5791ef2351a22cb6c
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-11-10 16:17:18 +0800

    Added catch to Swal.fire call.

--- parkpasses/frontend/parkpasses/src/components/retailer/datatables/ReportsDatatable.vue
+++ parkpasses/frontend/parkpasses/src/components/retailer/datatables/ReportsDatatable.vue
@@ -369,7 +369,7 @@ export default {
                 if (result.isConfirmed) {
                     vm.updateProcessingStatus(id, reportNumber, 'P', 'paid');
                 }
-            })
+            }).catch(console.error).then(console.log);
         },
         markUnPaid: function (id, reportNumber) {
             let vm = this;
@@ -384,7 +384,7 @@ export default {
                 if (result.isConfirmed) {
                     vm.updateProcessingStatus(id, reportNumber, 'U', 'unpaid');
                 }
-            })
+            }).catch(console.error).then(console.log);
         },
         addEventListeners: function(){
             let vm = this
