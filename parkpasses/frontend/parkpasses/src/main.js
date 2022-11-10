9bd646d44fdcc00c98595d08351c4cb5f7a5fc55
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-11-10 16:10:03 +0800

    Added catch to the router isRead call.

--- parkpasses/frontend/parkpasses/src/main.js
+++ parkpasses/frontend/parkpasses/src/main.js
@@ -62,7 +62,7 @@ app.use(CKEditor);
 app.use(router);
 app.use(pinia);
 
-router.isReady().then(() => app.mount('#app'));
+router.isReady().then(() => app.mount('#app')).catch(console.error).then(console.log);
 
 const store = useStore();
 store.fetchUserData();
