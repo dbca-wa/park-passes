5b0167391f7542c8ed1e99850c15d597459c0ec0
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-11-14 14:12:58 +0800

    Added logging to user_logged_in signal methods.

--- parkpasses/components/users/signals.py
+++ parkpasses/components/users/signals.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.auth.signals import user_logged_in
 
 from parkpasses.components.cart.models import Cart
@@ -5,15 +7,19 @@ from parkpasses.components.passes.models import Pass
 from parkpasses.components.retailers.models import RetailerGroupUser
 from parkpasses.helpers import is_retailer
 
+logger = logging.getLogger(__name__)
+
 
 def init_cart(sender, user, request, **kwargs):
     """Since items can be added to a cart anonymously
     we need to attach the correct user to those items once they log in"""
+    logger.info("user_logged_in signal running init_cart function")
     cart = Cart.get_or_create_cart(request)
     cart.set_user_for_cart_and_items(user.id)
 
 
 def add_retailer_to_session(sender, user, request, **kwargs):
+    logger.info("user_logged_in signal running add_retailer_to_session function")
     if is_retailer(request):
         retailer_group_user = (
             RetailerGroupUser.objects.filter(emailuser=user.id)
@@ -32,6 +38,7 @@ def add_retailer_to_session(sender, user, request, **kwargs):
 def assign_orphan_passes(sender, user, request, **kwargs):
     """When passes are sold via retailers, the user doesn't necessarily have an account in ledger
     so we need a way to attach the user to those passes when they log in"""
+    logger.info("user_logged_in signal running assign_orphan_passes function")
     queryset = Pass.objects.filter(user__isnull=True, email=user.email)
     if queryset.exists():
         for park_pass in queryset:
