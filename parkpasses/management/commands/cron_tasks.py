db4b69387b85e8dc1728c09408b4acc3f303bb5d
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-11-14 14:14:10 +0800

    Configured all the cron tasks that should run on a daily basis.

--- parkpasses/management/commands/cron_tasks.py
+++ parkpasses/management/commands/cron_tasks.py
@@ -3,67 +3,85 @@ import subprocess
 from pathlib import Path
 
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.core.management.base import BaseCommand
 
 logger = logging.getLogger(__name__)
 
-LOGFILE = "logs/cron_tasks.log"
+cron_email = logging.getLogger("cron_email")
+
+LOGFILE = "logs/" + settings.CRON_EMAIL_FILE_NAME  # This file is used temporarily.
+# It's cleared whenever this cron starts, then at the end the contents of this file is emailed.
 
 
 class Command(BaseCommand):
-    help = "Run the Commercial Operator Cron tasks"
+    help = "Run the Park Passes Cron tasks"
 
     def handle(self, *args, **options):
         stdout_redirect = f" | tee -a {LOGFILE}"
         subprocess.call(f"cat /dev/null > {LOGFILE}", shell=True)  # empty the log file
 
-        logger.info(f"Running command {__name__}")
+        logger.info(f"Running command {__name__}\n\n")
+
+        logger.info("Running python manage.py parkpasses_check\n\n")
         subprocess.call(
-            "python manage_co.py update_compliance_status" + stdout_redirect, shell=True
+            "python manage.py parkpasses_check" + stdout_redirect, shell=True
+        )
+        logger.info(
+            "Running python manage.py pass_send_vehicle_details_not_provided_notification_emails\n\n"
         )
         subprocess.call(
-            "python manage_co.py send_compliance_reminder" + stdout_redirect, shell=True
+            "python manage.py pass_send_vehicle_details_not_provided_notification_emails"
+            + stdout_redirect,
+            shell=True,
+        )
+        logger.info(
+            "Running python manage.py pass_send_expired_notification_emails\n\n"
         )
         subprocess.call(
-            "python manage_co.py update_approval_status" + stdout_redirect, shell=True
+            "python manage.py pass_send_expired_notification_emails" + stdout_redirect,
+            shell=True,
+        )
+        logger.info(
+            "Running python manage.py pass_send_autorenew_and_expiry_notification_emails\n\n"
         )
         subprocess.call(
-            "python manage_co.py expire_approvals" + stdout_redirect, shell=True
+            "python manage.py pass_send_autorenew_and_expiry_notification_emails"
+            + stdout_redirect,
+            shell=True,
         )
+        logger.info("Running python manage.py pass_send_gold_pass_details_to_pica\n\n")
         subprocess.call(
-            "python manage_co.py approval_renewal_notices" + stdout_redirect, shell=True
+            "python manage.py pass_send_gold_pass_details_to_pica" + stdout_redirect,
+            shell=True,
         )
+        logger.info("Running python manage.py clear_expired_sessions\n\n")
         subprocess.call(
-            "python manage_co.py eclass_expiry_notices" + stdout_redirect, shell=True
-        )
-        subprocess.call(
-            "python manage_co.py eclass_renewal_notices" + stdout_redirect, shell=True
-        )
-        subprocess.call(
-            "python manage_co.py monthly_invoices" + stdout_redirect, shell=True
-        )
-        subprocess.call(
-            "python manage_co.py update_cache" + stdout_redirect, shell=True
+            "python manage.py clear_expired_sessions" + stdout_redirect, shell=True
         )
 
         logger.info(f"Command {__name__} completed")
         self.send_email()
 
     def send_email(self):
-        log_txt = Path(LOGFILE).read_text()
+        email_instance = settings.EMAIL_INSTANCE
+        contents_of_cron_email = Path(LOGFILE).read_text()
         subject = f"{settings.SYSTEM_NAME_SHORT} - Cronjob"
-        body = ""
         to = (
             settings.CRON_NOTIFICATION_EMAIL
             if isinstance(settings.NOTIFICATION_EMAIL, list)
             else [settings.CRON_NOTIFICATION_EMAIL]
         )
-        send_mail(
+        msg = EmailMultiAlternatives(
             subject,
-            body,
+            contents_of_cron_email,
             settings.EMAIL_FROM,
             to,
-            fail_silently=False,
-            html_message=log_txt,
+            headers={"System-Environment": email_instance},
         )
+        msg.attach_alternative(contents_of_cron_email, "text/html")
+        msg.send()
+
+    def clear_cron_email_log(self):
+        with open(LOGFILE, "w"):
+            pass
