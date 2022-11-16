1f2ae9ce3d71dbbf1d7d46f81995c0bbab537194
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-11-16 11:34:12 +0800

    Removing org model documents and org model logs will install them from git.

--- org_model_logs/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrgModelLogsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "org_model_logs"
-    verbose_name = "Park Pass Model Logs"
