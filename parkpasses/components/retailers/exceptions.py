47224a0179ba39061afb1dd31b121ac192f1427c
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-12-22 15:44:09 +0800

    Added MultipleRACRetailerGroupsExist and NoRACRetailerGroupExists exceptions.:

--- parkpasses/components/retailers/exceptions.py
+++ parkpasses/components/retailers/exceptions.py
@@ -10,3 +10,11 @@ class MultipleDBCARetailerGroupsExist(Exception):
 
 class NoDBCARetailerGroupExists(Exception):
     """The exception to be thrown if there is no retailer group containing 'DBCA'"""
+
+
+class MultipleRACRetailerGroupsExist(Exception):
+    """The exception to be thrown if there is more than one retailer group containing 'RAC'"""
+
+
+class NoRACRetailerGroupExists(Exception):
+    """The exception to be thrown if there is no retailer group containing 'RAC'"""
