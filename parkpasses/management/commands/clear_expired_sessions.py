0569bd8e24edb0afefef1594b5249c762c50a372
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-01-06 10:04:19 +0800

    Added code to delete any passes that are still in a cart but have expired.

--- parkpasses/management/commands/clear_expired_sessions.py
+++ parkpasses/management/commands/clear_expired_sessions.py
@@ -12,6 +12,7 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 
 from parkpasses.components.cart.models import Cart
+from parkpasses.components.passes.models import Pass
 
 
 class Command(BaseCommand):
@@ -19,6 +20,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         now = timezone.now()
+        date_today = now.date()
         two_weeks_ago = now - timezone.timedelta(days=14)
 
         self.stdout.write(
@@ -33,8 +35,35 @@ class Command(BaseCommand):
                 for expired_cart_item in expired_cart_items:
                     expired_cart_item.delete()
                 expired_cart.delete()
+            self.stdout.write(
+                self.style.SUCCESS(f"Deleted {len(expired_carts)} Expired carts.")
+            )
         else:
-            self.stdout.write("No expired park passes carts found.")
+            self.stdout.write(
+                "No park passes that were added to a cart more than two weeks ago found."
+            )
+
+        # A pass may have been in a cart less than 2 weeks but has already expired
+        # (i.e. 5 day holiday pass or any short duration pass)
+        # So we need to check for those daily and remove them so they are not accidentally purchased.
+        self.stdout.write(
+            "Selecting any passes that are still in a cart but have expired"
+        )
+        expired_passes = Pass.objects.exclude(
+            cancellation__isnull=False,  # to exclude cancelled passes
+        ).filter(
+            in_cart=True,
+            date_expiry__lte=date_today,
+        )
+        if expired_passes.exists():
+            for expired_pass in expired_passes:
+                expired_pass.delete()
+
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Deleted {len(expired_passes)} Expired passes still in carts."
+                )
+            )
 
         self.stdout.write("Clearing expired django sessions.")
         engine = import_module(settings.SESSION_ENGINE)
