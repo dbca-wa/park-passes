7dcbcf9993142f9ac40ae0157a1d882dc89b29ef
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-01-31 16:36:20 +0800

    Further tweaks of park pass pdf generation.

--- parkpasses/components/passes/utils.py
+++ parkpasses/components/passes/utils.py
@@ -12,8 +12,7 @@ from django.conf import settings
 from django.utils import timezone
 from django.utils.dateformat import DateFormat
 from django.utils.text import slugify
-from docx.shared import Mm
-from docxtpl import DocxTemplate, InlineImage
+from docxtpl import DocxTemplate, RichText
 
 logger = logging.getLogger(__name__)
 
@@ -22,13 +21,8 @@ class PassUtils:
     def generate_pass_pdf_from_docx_template(
         self, park_pass, pass_template, qr_code_path
     ):
-
         park_pass_docx = DocxTemplate(
             f"{settings.PROTECTED_MEDIA_ROOT}/{pass_template.template.name}"
-        )
-
-        qr_image = InlineImage(
-            park_pass_docx, image_descriptor=qr_code_path, width=Mm(40)
         )
 
         date_format = DateFormat(park_pass.date_start)
@@ -45,44 +39,84 @@ class PassUtils:
 
         template_image = settings.PASS_TEMPLATE_DEFAULT_IMAGE_PATH
 
+        if pass_type.template_image:
+            template_image = pass_type.template_image.path
+
         concession_card_type = None
         concession_card_number = None
+
+        display_name_colour = pass_type.display_name_colour
+
         if hasattr(park_pass, "concession_usage"):
             concession_usage = park_pass.concession_usage
             concession_card_number = concession_usage.concession_card_number
             concession_card_type = concession_usage.concession.concession_type
+            display_name_colour = pass_type.concession_display_name_colour
             if pass_type.concession_template_image:
                 template_image = pass_type.concession_template_image.path
-        else:
-            if pass_type.template_image:
-                template_image = pass_type.template_image.path
 
         # This line replaces the background image in the docx file
         park_pass_docx.replace_zipname(
             settings.PASS_TEMPLATE_REPLACEMENT_IMAGE_PATH, template_image
         )
 
+        park_group = None
+        if park_pass.park_group:
+            park_group = park_pass.park_group.name
+
         order_number = None
         order = park_pass.order
         if order:
             order_number = order.order_number
 
-        context = {
-            "pass_qr_code": qr_image,
-            "pass_number": park_pass.pass_number,
-            "pass_type": pass_type_display_name,
-            "pass_concession_card_number": concession_card_number,
-            "pass_concession_card_type": concession_card_type,
-            "pass_start": date_start,
-            "pass_expiry": date_expiry,
-            "pass_first_name": park_pass.first_name,
-            "pass_last_name": park_pass.last_name,
-            "pass_vehicle_registration_1": park_pass.vehicle_registration_1,
-            "pass_vehicle_registration_2": park_pass.vehicle_registration_2,
-            "pass_drivers_licence_number": park_pass.drivers_licence_number,
-            "pass_order_number": order_number,
-            "pass_purchase_date": datetime_created,
-        }
+        rider_name = f"{park_pass.first_name} {park_pass.last_name}"
+
+        rich_text_pass_type_display_name = RichText(
+            pass_type_display_name,
+            color=display_name_colour,
+            size=28,
+            bold=True,
+        )
+
+        if settings.USE_DUMMY_QR_CODE_DATA:
+            # In a test environment, we don't add any personal data into the pass context
+            context = {
+                "pass_type": rich_text_pass_type_display_name,
+                "pass_park_group": park_group,
+                "pass_postcode": park_pass.postcode,
+                "pass_number": park_pass.pass_number,
+                "pass_concession_card_number": "CONC123123",
+                "pass_concession_card_type": concession_card_type,
+                "pass_start": date_start,
+                "pass_expiry": date_expiry,
+                "pass_vehicle_registration_1": "VehicleReg1",
+                "pass_vehicle_registration_2": "VehicleReg2",
+                "pass_drivers_licence_number": "L12312312",
+                "pass_first_name": "John",
+                "pass_last_name": "Smith",
+                "pass_rider_name": "John Smith",
+                "pass_purchase_date": datetime_created,
+                "pass_order_number": order_number,
+            }
+        else:
+            context = {
+                "pass_type": rich_text_pass_type_display_name,
+                "pass_park_group": park_group,
+                "pass_postcode": park_pass.postcode,
+                "pass_number": park_pass.pass_number,
+                "pass_concession_card_number": concession_card_number,
+                "pass_concession_card_type": concession_card_type,
+                "pass_start": date_start,
+                "pass_expiry": date_expiry,
+                "pass_vehicle_registration_1": park_pass.vehicle_registration_1,
+                "pass_vehicle_registration_2": park_pass.vehicle_registration_2,
+                "pass_drivers_licence_number": park_pass.drivers_licence_number,
+                "pass_first_name": park_pass.first_name,
+                "pass_last_name": park_pass.last_name,
+                "pass_rider_name": rider_name,
+                "pass_purchase_date": datetime_created,
+                "pass_order_number": order_number,
+            }
 
         park_pass_docx.render(context)
 
@@ -107,6 +141,7 @@ class PassUtils:
         output = subprocess.check_output(
             [
                 "libreoffice",
+                # "--headless",
                 "--convert-to",
                 "pdf",
                 park_pass_docx_full_file_path,
@@ -130,7 +165,7 @@ class PassUtils:
 
         park_pass.park_pass_pdf.name = new_path
 
-        image_rectangle = fitz.Rect(410, 70, 530, 190)
+        image_rectangle = fitz.Rect(409, 69, 529, 189)
         file_handle = fitz.open(park_pass.park_pass_pdf.path)
         first_page = file_handle[0]
 
@@ -138,7 +173,7 @@ class PassUtils:
         file_handle.saveIncr()
 
         # Clean up unused files
-        os.remove(park_pass_docx_full_file_path)
+        # os.remove(park_pass_docx_full_file_path)
 
         logger.info(qr_code_path)
         # os.remove(qr_code_path)
