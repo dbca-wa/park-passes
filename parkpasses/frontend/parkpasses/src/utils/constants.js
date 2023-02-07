a40e061558ee7f95d43f4ba190d91ebed3113787
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-02-07 14:07:34 +0800

    Made SYSTEM a getter so it can refer to PARK_PASSES_SUPPORT_EMAIL property.

--- parkpasses/frontend/parkpasses/src/utils/constants.js
+++ parkpasses/frontend/parkpasses/src/utils/constants.js
@@ -33,8 +33,10 @@ module.exports = {
 
     ERRORS: {
         NETWORK: 'NETWORK ERROR: Make sure your internet connection is working and try again.',
-        SYSTEM: `SYSTEM ERROR: An error has occured accessing the Park Passes API. Please try again \
-        in an hour and if the problem persists contact us at: ${PARK_PASSES_SUPPORT_EMAIL}`,
+        get SYSTEM() {
+            return `SYSTEM ERROR: An error has occured accessing the Park Passes API. Please try again \
+            in an hour and if the problem persists contact us at: ${this.PARK_PASSES_SUPPORT_EMAIL}`;
+        },
         // The critical error will be shown on the client side when a critical error
         // has occured on the backend and needs the attention of the system admins.
         CRITICAL: 'SYSTEM ERROR: Our System Administrators have been notified. Please try again in an hour.'
