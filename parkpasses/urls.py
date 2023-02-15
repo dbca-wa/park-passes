1fb79bfa14da8ac42ebde08645317cb0fd0f5db7
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-02-15 14:28:36 +0800

    Only include debug toolbar urls if settings.SHOW_DEBUG_TOOLBAR is True.

--- parkpasses/urls.py
+++ parkpasses/urls.py
@@ -235,7 +235,8 @@ urlpatterns = [
 if settings.DEBUG:  # Serve media locally in development.
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    urlpatterns += [
-        # ...
-        path("__debug__/", include("debug_toolbar.urls")),
-    ]
+    if settings.SHOW_DEBUG_TOOLBAR:
+        urlpatterns += [
+            # ...
+            path("__debug__/", include("debug_toolbar.urls")),
+        ]
