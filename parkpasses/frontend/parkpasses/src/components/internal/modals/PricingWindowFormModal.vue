71db471e23170adf4c65e34552765f65c137ac59
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-02-16 15:10:58 +0800

    Bug fix: Pass type validation was giving a tick when a pass type wasn't selected now will complain if no pass type has been selected.

--- parkpasses/frontend/parkpasses/src/components/internal/modals/PricingWindowFormModal.vue
+++ parkpasses/frontend/parkpasses/src/components/internal/modals/PricingWindowFormModal.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="modal fade" id="pricingWindowModal" tabindex="-1" aria-labelledby="pricingWindowModalLabel" aria-hidden="true">
+    <div class="modal fade" id="pricingWindowModal" tabindex="-1" aria-labelledby="pricingWindowModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <form @submit.prevent="validateForm" @keydown.enter="$event.preventDefault()" class="needs-validation" novalidate>
+                <form @submit.prevent="validateForm" @keydown.enter="$event.preventDefault()" class="needs-validation"
+                    novalidate>
                     <div class="modal-header">
                         <h5 class="modal-title" id="pricingWindowModalLabel">Add a New Pricing Window</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -10,12 +12,12 @@
                     <div class="modal-body">
                         <div class="mb-3">
                             <label for="pass-type" class="col-form-label">Pass Type:</label>
-                            <select @change="fetchDefaultOptionsForPassType" class="form-control" :class="errors.pass_type ? 'is-invalid' : ''"
-                                v-model="pricingWindow.pass_type" aria-describedby="validationServerPassTypeFeedback"
-                                required>
-                                <option value="0" selected="selected" disabled="disabled">Select a Pass Type</option>
+                            <select @change="fetchDefaultOptionsForPassType" ref="passType" class="form-control"
+                                :class="errors.pass_type ? 'is-invalid' : ''" v-model="pricingWindow.pass_type"
+                                aria-describedby="validationServerPassTypeFeedback" required>
+                                <option disabled value="0">Select a Pass Type</option>
                                 <option v-for="passType in passTypesDistinct" :value="passType.code">{{
-                                        passType.description
+                                    passType.description
                                 }}</option>
                             </select>
                             <div v-if="errors.pass_type" id="validationServerPassTypeFeedback" class="invalid-feedback">
@@ -28,8 +30,8 @@
                         <div class="mb-3">
                             <label for="name" class="col-form-label">Name:</label>
                             <input type="text" class="form-control" :class="errors.name ? 'is-invalid' : ''" id="name"
-                                name="name" v-model="pricingWindow.name"
-                                aria-describedby="validationServerNameFeedback" required />
+                                name="name" v-model="pricingWindow.name" aria-describedby="validationServerNameFeedback"
+                                required />
                             <div v-if="errors.name" id="validationServerNameFeedback" class="invalid-feedback">
                                 <p v-for="(error, index) in errors.name" :key="index">{{ error }}</p>
                             </div>
@@ -40,10 +42,11 @@
                         <div class="mb-3">
                             <label for="dateStart" class="col-form-label">Start Date</label>
                             <input @change="updateMinEndDate()" type="date" class="form-control"
-                                :class="errors.date_start ? 'is-invalid' : ''" id="dateStart"
-                                name="dateStart" v-model="pricingWindow.date_start" required="required"
-                                :min="defaultStartDate()" aria-describedby="validationServerDateStartFeedback">
-                            <div v-if="errors.date_start"  id="validationServerDateStartFeedback" class="invalid-feedback">
+                                :class="errors.date_start ? 'is-invalid' : ''" id="dateStart" name="dateStart"
+                                v-model="pricingWindow.date_start" required="required" :min="defaultStartDate()"
+                                aria-describedby="validationServerDateStartFeedback">
+                            <div v-if="errors.date_start" id="validationServerDateStartFeedback"
+                                class="invalid-feedback">
                                 <p v-for="(error, index) in errors.date_start" :key="index">{{ error }}</p>
                             </div>
                             <div v-else id="validationServerDateStartFeedback" class="invalid-feedback">
@@ -52,11 +55,12 @@
                         </div>
                         <div class="mb-3">
                             <label for="dateExpiry" class="col-form-label">End Date</label>
-                            <input type="date" class="form-control"
-                                :class="errors.date_expiry ? 'is-invalid' : ''" id="dateExpiry"
-                                name="dateExpiry" v-model="pricingWindow.date_expiry" required="required"
-                                :min="minEndDate" aria-describedby="validationServerDateExpiryFeedback">
-                            <div v-if="errors.date_expiry"  id="validationServerDateExpiryFeedback" class="invalid-feedback">
+                            <input type="date" class="form-control" :class="errors.date_expiry ? 'is-invalid' : ''"
+                                id="dateExpiry" name="dateExpiry" v-model="pricingWindow.date_expiry"
+                                required="required" :min="minEndDate"
+                                aria-describedby="validationServerDateExpiryFeedback">
+                            <div v-if="errors.date_expiry" id="validationServerDateExpiryFeedback"
+                                class="invalid-feedback">
                                 <p v-for="(error, index) in errors.date_expiry" :key="index">{{ error }}</p>
                             </div>
                             <div v-else id="validationServerDateExpiryFeedback" class="invalid-feedback">
@@ -64,8 +68,9 @@
                             </div>
                         </div>
                         <div v-if="defaultPassOptions">
-                         <label for="" class="col-form-label">Options</label>
-                            <table v-if="defaultPassOptions && defaultPassOptions.length > 0" class="table table-striped">
+                            <label for="" class="col-form-label">Options</label>
+                            <table v-if="defaultPassOptions && defaultPassOptions.length > 0"
+                                class="table table-striped">
                                 <thead>
                                     <tr>
                                         <th>Name</th>
@@ -78,7 +83,8 @@
                                         <td>{{ option.name }}</td>
                                         <td>{{ option.duration }} days</td>
                                         <td>
-                                            <input type="text" class="form-control" name="options" v-model="pricingWindow.pricing_options[index]" required="required" />
+                                            <input type="text" class="form-control" name="options"
+                                                v-model="pricingWindow.pricing_options[index]" required="required" />
                                             <div id="validationOptionsFeedback" class="invalid-feedback">
                                                 You must specify a price for this pricing option.
                                             </div>
@@ -119,7 +125,7 @@ export default {
     computed: {
         startDate: function () {
             let startDate;
-            if(this.pricingWindow.date_start) {
+            if (this.pricingWindow.date_start) {
                 startDate = new Date(this.pricingWindow.date_start);
             } else {
                 startDate = new Date();
@@ -153,26 +159,25 @@ export default {
         },
         fetchDefaultOptionsForPassType: function () {
             let vm = this;
-            console.log('vm.pricingWindow.pass_type = ' + vm.pricingWindow.pass_type);
             fetch(apiEndpoints.defaultPassOptions(vm.pricingWindow.pass_type))
-            .then(async response => {
-                const data = await response.json();
-                if (!response.ok) {
-                    const error = (data && data.message) || response.statusText;
-                    console.log(error)
-                    return Promise.reject(error);
-                }
-                if(data.results.length > 0) {
-                    vm.defaultPassOptions = data.results
-                } else {
-                    this.systemErrorMessage = constants.ERRORS.CRITICAL;
-                    console.error(`SYSTEM ERROR: Unable to load options for pass type id: ${vm.passTypeId}`);
-                }
-            })
-            .catch(error => {
-                this.systemErrorMessage = constants.ERRORS.SYSTEM;
-                console.error("There was an error!", error);
-            });
+                .then(async response => {
+                    const data = await response.json();
+                    if (!response.ok) {
+                        const error = (data && data.message) || response.statusText;
+                        console.log(error)
+                        return Promise.reject(error);
+                    }
+                    if (data.results.length > 0) {
+                        vm.defaultPassOptions = data.results
+                    } else {
+                        this.systemErrorMessage = constants.ERRORS.CRITICAL;
+                        console.error(`SYSTEM ERROR: Unable to load options for pass type id: ${vm.passTypeId}`);
+                    }
+                })
+                .catch(error => {
+                    this.systemErrorMessage = constants.ERRORS.SYSTEM;
+                    console.error("There was an error!", error);
+                });
         },
         submitForm: function () {
             let vm = this;
@@ -200,22 +205,28 @@ export default {
                 })
 
                 this.$emit("saveSuccess", {
-                        message: 'Pricing Window created successfully.',
-                        pricingWindow: data,
-                    }
+                    message: 'Pricing Window created successfully.',
+                    pricingWindow: data,
+                }
                 );
                 vm.pricingWindow = vm.getPricingWindowInitialState();
                 var PricingWindowFormModalModal = bootstrap.Modal.getInstance(document.getElementById('pricingWindowModal'));
                 PricingWindowFormModalModal.hide();
             })
-            .catch(error => {
-                this.systemErrorMessage = constants.ERRORS.SYSTEM;
-                console.error("There was an error!", error);
-            });
+                .catch(error => {
+                    this.systemErrorMessage = constants.ERRORS.SYSTEM;
+                    console.error("There was an error!", error);
+                });
             return false;
         },
         validateForm: function () {
             let vm = this;
+            console.log('vm.pricingWindow.pass_type = ' + vm.pricingWindow.pass_type)
+            if (0 == vm.pricingWindow.pass_type) {
+                vm.$refs.passType.setCustomValidity("Invalid field.");
+            } else {
+                vm.$refs.passType.setCustomValidity("");
+            }
             var forms = document.querySelectorAll('.needs-validation');
             Array.prototype.slice.call(forms)
                 .forEach(function (form) {
